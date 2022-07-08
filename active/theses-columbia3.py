8689306f82a64391e4b919ad66e909801f60caab
Florian Schwennsen <florian.schwennsen@desy.de>
2022-07-08 14:08:43 +0200

    theses-columbia3: new harvester for Columbia U.

---
+++ active/theses-columbia3.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#harvest theses from Columbia U. 
+#FS: 2020-03-23
+
+
+import getopt
+import sys
+import os
+import urllib.request, urllib.error, urllib.parse
+import urllib.parse
+from bs4 import BeautifulSoup
+import re
+import ejlmod3
+import time
+
+publisher = 'Columbia U.'
+jnlfilename = 'THESES-COLUMBIA-%s' % (ejlmod3.stampoftoday())
+
+rpp = 20
+deps = ['Applied+Physics+and+Applied+Mathematics', 'Mathematics', 'Physics']
+
+hdr = {'User-Agent' : 'Magic Browser'}
+recs = []
+for dep in deps:
+    tocurl = 'https://academiccommons.columbia.edu/search?f%5Bdegree_grantor_ssim%5D%5B%5D=%28%22Columbia+University%22+OR+%22Teachers+College%2C+Columbia+University%22+OR+%22Union+Theological+Seminary%22+OR+%22Mailman+School+of+Public+Health%2C+Columbia+University%22%29&f%5Bdegree_level_name_ssim%5D%5B%5D=Doctoral&f%5Bdepartment_ssim%5D%5B%5D=' + dep + '&f%5Bgenre_ssim%5D%5B%5D=Theses&per_page=' + str(rpp) + '&sort=Published+Latest'
+    req = urllib.request.Request(tocurl, headers=hdr)
+    tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
+    time.sleep(2)
+    for h3 in tocpage.body.find_all('h3', attrs = {'class' : 'index_title'}):
+        for a in h3.find_all('a'):
+            rec = {'tc' : 'T', 'jnl' : 'BOOK', 'note' : [], 'keyw' : [], 'supervisor' : []}
+            rec['link'] = 'https://academiccommons.columbia.edu' + a['href']
+            rec['note'].append(re.sub('\W', ' ', dep))
+            if dep == 'Mathematics':
+                rec['fc'] = 'm'
+            recs.append(rec)
+
+#check individual thesis pages
+i = 0
+for rec in recs:
+    i += 1
+    ejlmod3.printprogress('-', [[i, len(recs)], [rec['link']]])
+    try:
+        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
+        time.sleep(3)
+    except:
+        try:
+            print("retry %s in 180 seconds" % (rec['link']))
+            time.sleep(180)
+            artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
+        except:
+            print("no access to %s" % (rec['link']))
+            continue
+    ejlmod3.globallicensesearch(rec, artpage)
+    ejlmod3.metatagcheck(rec, artpage, ['citation_title', 'citation_author', 'citation_publication_date', 'citation_doi', 'citation_keywords', 'description', 'citation_pdf_url'])
+    rec['autaff'][-1].append(publisher)
+    #supervisor
+    for dl in artpage.body.find_all('dl'):
+        for child in dl.children:
+            try:
+                child.name
+            except:
+                continue
+            if child.name == 'dt':
+                dtt = child.text.strip()
+            elif child.name == 'dd' and dtt == 'Thesis Advisors':
+                rec['supervisor'].append([child.text.strip()])
+    ejlmod3.printrecsummary(rec)
+    
+ejlmod3.writenewXML(recs, publisher, jnlfilename)
