41a354c21295b14bc8b539f10a6a18d719b8c6aa
Florian Schwennsen <florian.schwennsen@desy.de>
2022-09-05 08:45:30 +0200

    theses-puebla3: new code to harvest theses from Puebla U.

---
+++ active/theses-puebla3.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+#harvest theses from Puebla U.
+#JH: 2022-06-07
+#FS: 2022-09-04
+
+
+import getopt
+import sys
+import os
+import codecs
+import datetime
+import ejlmod3
+import undetected_chromedriver as uc
+#from selenium import webdriver
+from bs4 import BeautifulSoup
+from time import sleep
+
+# Initiate webdriver
+#driver = webdriver.PhantomJS()
+options = uc.ChromeOptions()
+options.headless=True
+options.binary_location='/usr/bin/chromium-browser'
+options.add_argument('--headless')
+driver = uc.Chrome(version_main=103, options=options)
+
+
+publisher = 'Puebla U., Inst. Fis.'
+jnlfilename = 'THESES-PUEBLA-%s' % (ejlmod3.stampoftoday())
+
+recs = []
+
+rpp = 50
+pages = 2
+
+boring = ['Facultad de Ciencias Químicas', 'Área de Ciencias Sociales y Humanidades',
+          'Área Económico Administrativa', 'Área de Ciencias Sociales',
+          'Área de Educación y Humanidades']
+boring += ['Facultad de Economía', 'Facultad de Derecho y Ciencias Sociales',
+           'Facultad de Arquitectura', 'Instituto de Fisiología',
+           'nstituto de Ciencias de Gobierno y Desarrollo Estratégico<',
+           'Facultad de Filosofía y Letras']
+
+def get_sub_site(url):
+    global uninterestingDOIS
+    keepit = True
+    print('[%s] --> Harversting Data' % url)
+    driver.get(url)
+
+    rec = {'tc': 'T', 'jnl': 'BOOK', 'supervisor': [], 'autaff': [], 'keyw': [], 'link' : url, 'note' : []}
+
+    artpage = BeautifulSoup(driver.page_source, 'lxml')
+    ejlmod3.metatagcheck(rec, artpage, ['DC.rights', 'DC.subject', 'citation_title', 'citation_language',
+                                        'citation_date', 'DC.identifier', 'DCTERMS.abstract',
+                                        'citation_title', 'citation_pdf_url'])
+
+    rows = artpage.find_all('tr', attrs={'class': 'ds-table-row'})
+    for row in rows:
+        columns = row.find_all('td')
+        label = columns[0].text
+        data = columns[1].text
+
+        # Get the supervisor
+        if label == 'dc.contributor.advisor':
+            rec['supervisor'].append([data.split(';')[0]])
+
+        # Get the author
+        elif label == 'dc.contributor.author':
+            rec['autaff'].append([data, publisher])
+
+        # Check if it is really a PhD
+        elif label == 'dc.type.degree':
+            if data != 'Doctorado':
+                keepit = False
+
+        #department
+        elif label in ['dc.thesis.degreegrantor', 'dc.thesis.degreediscipline']:
+            if data in boring:
+                keepit = False
+            else:
+                rec['note'].append(label+': '+data)
+
+    if keepit:
+        recs.append(rec)
+        ejlmod3.printrecsummary(rec)
+    else:
+        ejlmod3.adduninterestingDOI(url)
+    return
+
+for page in range(pages):
+    to_curl = 'https://repositorioinstitucional.buap.mx/handle/20.500.12371/7/discover?rpp=' + str(rpp) + '&etal=0' \
+                                                                                                          '&group_by' \
+                                                                                                          '=none&page='\
+              + str(page+1) + '&sort_by=dc.date.issued_dt&order=desc '
+    ejlmod3.printprogress("=", [[page+1, pages], [to_curl]])
+    driver.get(to_curl)
+    for article in BeautifulSoup(driver.page_source, 'lxml').find_all('div', attrs={'class': 'col-sm-9 '
+                                                                                             'artifact-description'}):
+        article_link = article.find_all('a')
+        if len(article_link) == 1:
+            url = 'https://repositorioinstitucional.buap.mx%s?show=all' % article_link[0].get('href')
+            if ejlmod3.ckeckinterestingDOI(url):
+                get_sub_site(url)
+                sleep(4)
+    sleep(10)
+
+ejlmod3.writenewXML(recs, publisher, jnlfilename)
