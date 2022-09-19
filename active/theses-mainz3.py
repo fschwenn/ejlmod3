2a14db3d492b651ad305c2cfc068bba644c6ff5f
Florian Schwennsen <florian.schwennsen@desy.de>
2022-09-19 13:06:43 +0200

    theses-mainz3: new code to harvest theses from Mainz U.

---
+++ active/theses-mainz3.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+#harvest theses from Mainz U.
+#FS: 2020-01-27
+#FS: 2022-09-10
+
+import getopt
+import sys
+import os
+import urllib.request, urllib.error, urllib.parse
+from bs4 import BeautifulSoup
+import re
+import ejlmod3
+import time
+import json
+
+publisher = 'Mainz U.'
+
+jnlfilename = 'THESES-MAINZ-%s' % (ejlmod3.stampoftoday())
+hdr = {'User-Agent' : 'Magic Browser'}
+
+recs = []
+
+rpp = 40
+pages = 3
+
+for page in range(pages):
+    tocurl = 'https://openscience.ub.uni-mainz.de/simple-search?query=&filter_field_1=organisationalUnit&filter_type_1=equals&filter_value_1=FB+08+Physik%2C+Mathematik+u.+Informatik&filter_field_2=publicationType&filter_type_2=equals&filter_value_2=Dissertation&sort_by=dc.date.issued_dt&order=desc&rpp=' + str(rpp) + '&etal=0&start=' + str(page*rpp)
+    ejlmod3.printprogress('=', [[page+1, pages], [tocurl]])
+    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl), features="lxml")
+    for tr in tocpage.body.find_all('tr'):
+        rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK', 'note' : []}
+        for td in tr.find_all('td', attrs = {'headers' : 't3'}):
+            rec['year'] = td.text.strip()
+            rec['date'] = td.text.strip()
+        for td in tr.find_all('td', attrs = {'headers' : 't1'}):
+            for a in td.find_all('a'):
+                rec['tit'] = a.text.strip()
+                rec['hdl'] = re.sub('.*handle\/', '', a['href'])
+                rec['artlink'] = 'https://openscience.ub.uni-mainz.de' + a['href']
+                recs.append(rec)
+    time.sleep(10)
+
+i = 0
+for rec in recs:
+    i += 1
+    ejlmod3.printprogress('-', [[i, len(recs)], [rec['artlink']]])
+    try:
+        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['artlink']), features="lxml")
+        time.sleep(4)
+    except:
+        try:
+            print("retry %s in 180 seconds" % (rec['artlink']))
+            time.sleep(180)
+            artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['artlink']), features="lxml")
+        except:
+            print("no access to %s" % (rec['artlink']))
+            continue
+    for tr in artpage.body.find_all('tr'):
+        for td in tr.find_all('td', attrs = {'class' : 'metadataFieldLabel'}):
+            tdt = td.text.strip()
+        for td in tr.find_all('td', attrs = {'class' : 'metadataFieldValue'}):
+            #authors
+            if tdt == 'Authors:':
+                rec['autaff'] = [[ td.text.strip(), publisher ]]
+            #title
+            if tdt == 'Title:':
+                rec['tit'] = td.text.strip()
+            #language
+            elif tdt == 'Language :':
+                if td.text.strip() == 'german':
+                    rec['language'] = 'German'
+            #abstract
+            elif tdt == 'Abstract:':
+                rec['abs'] = td.text.strip()
+            #license
+            elif re.search('Information', tdt):
+                for a in td.find_all('a'):
+                    if re.search('creativecommons.org', a['href']):
+                        rec['license'] = {'url' : a['href']}
+            #pages
+            elif tdt == 'Extent:':
+                if re.search('\d\d', td.text):
+                    rec['pages'] = re.sub('.*?(\d\d+).*', r'\1', td.text.strip())
+            #DOI
+            elif tdt == 'DOI:':
+                for a in td.find_all('a'):
+                    rec['doi'] = re.sub('.*org\/', '', a['href'])                    
+        #FFT
+        for td in tr.find_all('td', attrs = {'class' : 'standard'}):
+            for a in td.find_all('a'):
+                if re.search('pdf$', a['href']):
+                    if 'license' in list(rec.keys()):
+                        rec['FFT'] = 'https://openscience.ub.uni-mainz.de' + a['href']
+                    else:
+                        rec['hidden'] = 'https://openscience.ub.uni-mainz.de' + a['href']
+    ejlmod3.printrecsummary(rec)
+
+ejlmod3.writenewXML(recs, publisher, jnlfilename)
