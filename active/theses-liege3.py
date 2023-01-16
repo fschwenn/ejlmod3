43e3c17058966f3a2852723c5491c322bd3b9726
Florian Schwennsen <florian.schwennsen@desy.de>
2023-01-16 08:26:19 +0100

    theses-liege3, acm3, theses-puebla3, theses-hkust3: get Chrome-Version directly from binary

--- active/theses-liege3.py
+++ active/theses-liege3.py
@@ -23,7 +23,8 @@ options = uc.ChromeOptions()
 options.headless=True
 options.binary_location='/usr/bin/chromium-browser'
 options.add_argument('--headless')
-driver = uc.Chrome(version_main=103, options=options)
+chromeversion = int(re.sub('Chro.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
 
 prerecs = []
 for (degree, authority1) in [('PhD', 'DSO%2FF03'), ('Habilitation', 'DSO%2FF04')]:
--- active/theses-puebla3.py
+++ active/theses-puebla3.py
@@ -21,7 +21,8 @@ options = uc.ChromeOptions()
 options.headless=True
 options.binary_location='/usr/bin/chromium-browser'
 options.add_argument('--headless')
-driver = uc.Chrome(version_main=103, options=options)
+chromeversion = int(re.sub('Chro.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
 
 
 publisher = 'Puebla U., Inst. Fis.'
