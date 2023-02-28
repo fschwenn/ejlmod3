06f9ae60ab487c582bde4a5e8b82ef7efe3b84be
Florian Schwennsen <florian.schwennsen@desy.de>
2023-02-28 15:12:25 +0100

    acm3: updated chrome setting

--- active/acm3.py
+++ active/acm3.py
@@ -69,10 +69,10 @@ else:
 print("get table of content... from %s" % (tocurl))
 
 options = uc.ChromeOptions()
-options.headless=True
 options.binary_location='/usr/bin/chromium-browser'
+options.binary_location='/usr/bin/google-chrome'
 options.add_argument('--headless')
-chromeversion = int(re.sub('Chro.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
 driver.implicitly_wait(300)
 
