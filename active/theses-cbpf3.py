b5d1094e49066334db2721253c31cc176d506030
Florian Schwennsen <florian.schwennsen@desy.de>
2023-03-15 16:15:52 +0100

    theses-cbpf3: new code to harvest theses from 'Rio de Janeiro, CBPF'

---
+++ active/theses-cbpf3.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+#harvest theses from Rio de Janeiro, CBPF
+#FS: 2020-03-26
+#FS: 2023-03-15
+
+import sys
+import os
+import urllib.request, urllib.error, urllib.parse
+from bs4 import BeautifulSoup
+import re
+import ejlmod3
+import time
+import mechanize
+
+publisher = 'Rio de Janeiro, CBPF'
+jnlfilename = 'THESES-CBPF-%s' % (ejlmod3.stampoftoday())
+
+pages = 1
+skipalreadyharvested = True
+
+tocurl = 'http://cbpfindex.cbpf.br/index.php?moduleFile=listPublications&pubType=9'
+
+br = mechanize.Browser()
+br.set_handle_robots(False)   # ignore robots
+br.set_handle_refresh(False)  # can sometimes hang without this
+br.addheaders = [('User-agent', 'Firefox')]
+response = br.open(tocurl)
+
+if skipalreadyharvested:
+    alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+
+recs = []
+print(tocurl)
+for page in range(pages):
+    tocpage = BeautifulSoup(response.read(), features='lxml')
+    ejlmod3.printprogress("=", [[page+1, pages]])
+    for a in tocpage.body.find_all('a', attrs = {'title' : 'Ver detalhes'}):
+        rec = {'tc' : 'T', 'jnl' : 'BOOK', 'supervisor' : []}        
+        rec['link'] = 'http://cbpfindex.cbpf.br/' + a['href']
+        rec['doi'] = '20.2000/CBPF/' + re.sub('\D', '', a['href'])
+        if skipalreadyharvested and rec['doi'] in alreadyharvested:
+            print('  ', rec['link'], 'already in backup')
+        else:
+            print('  ', rec['link'])
+            recs.append(rec)
+    #click to next page
+    br.form = list(br.forms())[2]
+    control = br.form.find_control('start')
+    control.readonly = False
+    control.value = "%i" % (16*page+32)
+    time.sleep(5)
+    response = br.submit()
+    
+i = 0
+for rec in recs:
+    i += 1
+    ejlmod3.printprogress("-", [[i, len(recs)], [rec['link']]])
+    try:
+        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features='lxml')
+        time.sleep(8)
+    except:
+        print("retry %s in 180 seconds" % (rec['link']))
+        time.sleep(180)
+        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features='lxml')
+    #get rid of associated articles
+    for fieldset in artpage.body.find_all('fieldset'):
+        for legend in fieldset.find_all('legend'):
+            if re.search('Artigo', legend.text):
+                fieldset.decompose()
+                print('  removed related articles from theses metadata page')
+    #title
+    for td in artpage.body.find_all('td', attrs = {'class' : 'pubTitle'}):
+        rec['tit'] = td.text.strip()
+    for td in artpage.body.find_all('td'):
+        parts = re.split(': *', td.text.strip())
+        if len(parts) == 2:
+            #date
+            if re.search('Publica', parts[0]):
+                if re.search('\d\d\/\d\d\/\d\d\d\d', parts[1]):
+                    rec['date'] = re.sub('.*(\d\d).(\d\d).(\d\d\d\d).*', r'\3-\2-\1', parts[1])
+                elif re.search('\d\d\d\d', parts[1]):
+                    rec['date'] = re.sub('.*(\d\d\d\d).*', r'\1', parts[1])
+            #author
+            elif re.search('Aluno', parts[0]):
+                rec['autaff'] = [[parts[1].strip(), publisher]]
+            #supervisor
+            elif re.search('Orientador', parts[0]):
+                rec['supervisor'].append([parts[1].strip()])
+            #department
+            #elif re.search('Institui', parts[0]):
+            #    rec['department'] = parts[1].strip()
+            #    rec['note'] = [rec['department']]
+            #abstract
+            elif re.search('Resum', parts[0]):
+                rec['abs'] = parts[1].strip()
+            #defense date
+            elif re.search('Data da defesa', parts[0]):
+                if re.search('\d\d\/\d\d\/\d\d\d\d', parts[1]):
+                    rec['MARC'] = [('500', [('a', re.sub('.*(\d\d).(\d\d).(\d\d\d\d).*', r'Presented on \3-\2-\1', parts[1]))])]
+                elif re.search('\d\d\d\d', parts[1]):
+                    rec['MARC'] = [('500', [('a', re.sub('.*(\d\d\d\d).*', r'Presented on \1', parts[1]))])]
+    #PDF
+    for a in artpage.body.find_all('a'):
+        for img in a.find_all('img'):
+            if re.search('Download do PDF', a.text):
+                rec['hidden'] = 'http://cbpfindex.cbpf.br/' + a['href']
+    ejlmod3.printrecsummary(rec)
+       
+
+ejlmod3.writenewXML(recs, publisher, jnlfilename)
