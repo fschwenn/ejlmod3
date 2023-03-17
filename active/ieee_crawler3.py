c9a420d33fce342aeb0926d19f3fbc817a963155
Florian Schwennsen <florian.schwennsen@desy.de>
2023-03-17 15:44:30 +0100

    ieee_crawler3:  updated chrome setting

--- active/ieee_crawler3.py
+++ active/ieee_crawler3.py
@@ -27,14 +27,14 @@ if host == 'l00schwenn':
     options.binary_location='/usr/bin/chromium'
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    driver = uc.Chrome(version_main=109, options=options)
+    chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+    driver = uc.Chrome(version_main=chromeversion, options=options)
     tmpdir = '/home/schwenn/tmp'
 else:
     options = uc.ChromeOptions()
     options.headless=True
 #    options.binary_location='/usr/bin/chromium-browser'
     options.add_argument('--headless')
-    #options.add_argument('--proxy-server=http://vpnbook:rxtasfh@fr1.vpnbook.com')
     #chromeversion = int(re.sub('Chro.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
     chromeversion = 108
     driver = uc.Chrome(version_main=chromeversion, options=options)
