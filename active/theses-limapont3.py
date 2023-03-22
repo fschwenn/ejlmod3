4703b60abe5d1310ea24e59d734425bafbca73eb
Florian Schwennsen <florian.schwennsen@desy.de>
2023-03-22 16:21:01 +0100

    theses-limapont3: major rewrite

--- active/theses-limapont3.py
+++ active/theses-limapont3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # harvest theses from Lima, Pont. U. Catolica
-# JH: 2019-01-23
+# JH: 2023-01-23
 
 from bs4 import BeautifulSoup
 from requests import Session
@@ -9,104 +9,130 @@ from urllib.parse import parse_qs
 import re
 import ejlmod3
 
-
-
-def dspace_extract_links(content: str):
-    soup = BeautifulSoup(content, 'lxml')
-    artifacts: list = soup.find_all('h4', attrs={'class': 'artifact-title'})
-    out: list = []
-
-    for artifact in artifacts:
-        out.append('https://tesis.pucp.edu.pe' + artifact.find_all('a')[0].get('href') + '?show=full')
-
-    return out
-
 publisher = 'Lima, Pont. U. Catolica'
 jnlfilename = 'THESES-LIMAPONT-%s' % (ejlmod3.stampoftoday())
-rpp = 20
-pages = 2
+rpp = 40
+pages = 8
+skipalreadyharvested = True
+boring = ['Administrac%C3%ADon+Estrat%C3%A9gica+de+Empresas',
+          'Administraci%C3%B3n+Estrat%C3%A9gica+de+Empresas',
+          'Antropolog%C3%ADa',
+          'Antropolog%C3%ADa+con+menci%C3%B3n+en+Estudios+Andinos',
+          'Ciencia+Pol%C3%ADtica+y+Gobierno', 'Ciencias+de+la+Educaci%C3%B3n', 'Derecho',
+          'Doctora+en+Ciencia+Pol%C3%ADtica+y+Gobierno',
+          'Doctor+en+Administraci%C3%B3n+Estrat%C3%A9gica+de+Empresas',
+          'Doctor+en+Antropolog%C3%ADa',
+          'Doctor+en+Antropolog%C3%ADa+con+menci%C3%B3n+en+Estudios+Andinos',
+          'Doctor+en+Ciencia+Pol%C3%ADtica+y+Gobierno',
+          'Doctor+en+Ciencias+de+la+Educaci%C3%B3n', 'Doctor+en+Derecho',
+          'Doctor+en+Econom%C3%ADa', 'Doctor+en+Filosof%C3%ADa',
+          'Doctor+en+Gesti%C3%B3n+Estrat%C3%A9gica+con+menci%C3%B3n+en+Gesti%C3%B3n+Empresarial+y+Sostenibilidad',
+          'Doctor+en+Gesti%C3%B3n+Estrat%C3%A9gica+con+menci%C3%B3n+en+Innovaci%C3%B3n+y+Gesti%C3%B3n+en+Educaci%C3%B3nr',
+          'Doctor+en+Historia', 'Doctor+en+Historia+con+menci%C3%B3n+en+Estudios+Andinos',
+          'Doctor+en+Humanidades',
+          'Doctor+en+Ling%C3%BC%C3%ADstica+con+menci%C3%B3n+en+Estudios+Andinos',
+          'Doctor+en+Literatura+Hispanoamericana', 'Doctor+en+Psicolog%C3%ADa',
+          'Doctor+en+Sociolog%C3%ADa', 'Econom%C3%ADa', 'Filosof%C3%ADa',
+          'Gesti%C3%B3n+Estrat%C3%A9gica+con+menci%C3%B3n+en+Gesti%C3%B3n+Empresarial+y+Sostenibilidad',
+          'Gesti%C3%B3n+Estrat%C3%A9gica+con+menci%C3%B3n+en+Innovaci%C3%B3n+y+Gesti%C3%B3n+en+Educaci%C3%B3n+Superior',
+          'Historia', 'Historia+con+menci%C3%B3n+en+Estudios+Andinos', 'Humanidades',
+          'Ling%C3%BC%C3%ADstica+con+menci%C3%B3n+en+Estudios+Andinos',
+          'Literatura+Hispanoamericana', 'Psicolog%C3%ADa', 'Sociolog%C3%ADa']
+
 recs: list = []
 
+if skipalreadyharvested:
+    alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
 with Session() as session:
+    prerecs = []
     for page in range(pages):
-        to_curl = 'https://tesis.pucp.edu.pe/repositorio/browse?rpp=' + str(rpp) + '&sort_by=2&type=dateissued&offset=' + str(rpp*page) + '&etal=-1&order=DESC'
+        to_curl = 'https://tesis.pucp.edu.pe/repositorio/handle/20.500.12404/1/browse?rpp=' + str(rpp) + '&sort_by=2&type=dateissued&offset=' + str(rpp*page) + '&etal=-1&order=DESC'
         print("#### OPEN PAGE %i/%i ####" % (page+1, pages))
         print(to_curl)
 
-        index_resp = session.get(to_curl)
+        try:
+            index_resp = session.get(to_curl)
+            if index_resp.status_code != 200:
+                print("Error: Can't open page!")
+                continue
+        except:
+            print('   try again in 60s')
+            sleep(60)
+            index_resp = session.get(to_curl)
 
-        if index_resp.status_code != 200:
-            print("Error: Can't open page!")
-            continue
+        tocpage = BeautifulSoup(index_resp.content.decode('utf-8'), 'lxml')
+        for rec in ejlmod3.getdspacerecs(tocpage, 'https://tesis.pucp.edu.pe', alreadyharvested=alreadyharvested):
+            keepit = True
+            for deg in rec['degrees']:
+                if deg == 'Matem%C3%A1ticas':
+                    rec['fc'] = 'm'
+                if deg in boring:
+                    keepit = False
+            if keepit:
+                prerecs.append(rec)
+        #links = dspace_extract_links(index_resp.content.decode('utf-8'))
+        print('  %4i records so far' % (len(prerecs)))
+        sleep(4)
 
-        #index_soup = BeautifulSoup(index_resp.content.decode('utf-8'), 'lxml')
-        #recs += ejlmod3.getdspacerecs(tocpage, 'https://tesis.pucp.edu.pe/repositorio')
-        links = dspace_extract_links(index_resp.content.decode('utf-8'))
 
-
+    for (i, rec) in enumerate(prerecs):
         jump = False
-        for link in links:
-            print('[%s] --> Harvesting data' % link)
-            rec: dict = {'tc': 'T', 'jnl': 'BOOK', 'autaff': [], 'supervisor': [], 'license': {}}
+        ejlmod3.printprogress('-', [[i+1, len(prerecs)], [rec['link']], [len(recs)]])
+        rec['supervisor'] = []
 
-            data_resp = session.get(link)
-
+        try:
+            data_resp = session.get(rec['link'])
+            sleep(2)
             if data_resp.status_code != 200:
-                print("[%s] --> Error: Can't open page! Skipping...." % link)
+                print("[%s] --> Error: Can't open page! Skipping...." % rec['link'])
                 continue
+        except:
+            print('   try again in 60s')
+            sleep(60)
+            data_resp = session.get(rec['link'])
+        site_soup = BeautifulSoup(data_resp.content.decode('utf-8'), 'lxml')
+        ejlmod3.metatagcheck(rec, site_soup, ['DC.creator', 'DCTERMS.issued', 'DC.identifier', 'DC.language', 'DC.subject', 'DC.title', 'DC.rights'])
+        rec['autaff'][-1].append(publisher)
 
-            site_soup = BeautifulSoup(data_resp.content.decode('utf-8'), 'lxml')
-            ejlmod3.metatagcheck(rec, site_soup, ['DC.creator', 'DCTERMS.issued', 'DC.identifier', 'DC.language', 'DC.subject', 'DC.title'])
+        for row in site_soup.find_all('tr', attrs={'class': 'ds-table-row'}):
+            cols = row.find_all('td')
 
-            for row in site_soup.find_all('tr', attrs={'class': 'ds-table-row'}):
-                cols = row.find_all('td')
+            title = cols[0].text
+            data = cols[1].text
 
-                title = cols[0].text
-                data = cols[1].text
+            # Get the supervisor
+            if title == 'dc.contributor.advisor':
+                rec['supervisor'].append([data])
+            # Get the abstract
+            if title == 'dc.description.abstract':
+                if data.find('the') != -1 and data.find('it') != -1:
+                    rec['abs'] = data
+                else:
+                    rec['abs'] = data
+                    rec['language'] = 'Spanish'
 
-                # Get the supervisor
-                if title == 'dc.contributor.advisor':
-                    rec['supervisor'].append([data])
+            # Get the level
+            if title == 'renati.level':
+                if data != 'https://purl.org/pe-repo/renati/level#doctor':
+                    jump = True
+                    print('   skip %s' % (data))
+                    
+        # Get the pdf file
+        pdf_section = site_soup.find_all('div', attrs={'class': 'file-link'})
+        if len(pdf_section) != 0:
+            pdf = pdf_section[0].find_all('a')
 
-                # Get the abstract
-                if title == 'dc.description.abstract':
-                    if data.find('the') != -1 and data.find('it') != -1:
-                        rec['abs'] = data
+            if len(pdf) == 1:
+                params = parse_qs('https://e-archivo.uc3m.es' + pdf[0].get('href'))
+                if len(params.get('isAllowed')) == 1:
+                    if params.get('isAllowed')[0] == 'y':
+                        rec['FFT'] = 'https://e-archivo.uc3m.es' + pdf[0].get('href')
+        if jump:
+            ejlmod3.adduninterestingDOI(rec['hdl'])
+        else:
+            recs.append(rec)
+            ejlmod3.printrecsummary(rec)
 
-                # Get the license
-                if title == 'dc.rights.uri':
-                    payload = data.replace('http://creativecommons.org/licenses/', '').split('/')
-
-                    rec['license']['url'] = data
-                    if len(payload) == 4:
-                        rec['license']['statement'] = payload[0].upper() + '-' + payload[1].upper()
-
-                # Get the level
-                if title == 'renati.level':
-                    if data != 'https://purl.org/pe-repo/renati/level#doctor':
-                        jump = True
-
-            # Get the pdf file
-            pdf_section = site_soup.find_all('div', attrs={'class': 'file-link'})
-            if len(pdf_section) != 0:
-                pdf = pdf_section[0].find_all('a')
-
-                if len(pdf) == 1:
-                    params = parse_qs('https://e-archivo.uc3m.es' + pdf[0].get('href'))
-
-                    if len(params.get('isAllowed')) == 1:
-                        if params.get('isAllowed')[0] == 'y':
-                            rec['FFT'] = 'https://e-archivo.uc3m.es' + pdf[0].get('href')
-            print(rec)
-            sleep(10)
-            if not jump:
-                recs.append(rec)
-                break
-            else:
-                print("[%s] --> Not a PhD theses! Skipping..." % link)
-                continue
-
-        break
-        sleep(10)
-
-ejlmod3.writenewXML(recs, publisher, jnlfilename, xmldir='dev/', retfilename='dev/')
+ejlmod3.writenewXML(recs, publisher, jnlfilename)
