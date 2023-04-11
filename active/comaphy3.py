381f86bceede74ebf1b981cf82fa4227de9e76bd
Florian Schwennsen <florian.schwennsen@desy.de>
2023-04-11 10:59:40 +0200

    comaphy3: new code to harvest journals from 'National Academy of Sciences of Ukraine'

---
+++ active/comaphy3.py
@@ -0,0 +1,101 @@
+# -*- coding: UTF-8 -*-
+#program to harvest 'Condensed Matter Phys.'
+# FS 2020-06-12
+# FS 2023-04-05
+
+#import os
+import ejlmod3
+import re
+import sys
+#import unicodedata
+#import string
+import urllib.request, urllib.error, urllib.parse
+from bs4 import BeautifulSoup
+import time
+        
+journal = sys.argv[1]
+vol = sys.argv[1]
+iss = sys.argv[2]
+publisher = 'National Academy of Sciences of Ukraine'
+
+jnlfilename =  'condmatphys%s.%s' % (vol, iss)
+
+#find TOC page
+starturl = 'http://www.icmp.lviv.ua/journal/Contents.html'
+startpage = BeautifulSoup(urllib.request.urlopen(starturl, timeout=300), features="lxml")
+searchstring = 'V.%s, %s' % (vol, iss)
+for a in startpage.find_all('a'):
+    if re.search(searchstring, a.text):
+        tocurl = 'http://www.icmp.lviv.ua/journal/' + a['href']
+        tocurltrunc = re.sub('(.*\/).*', r'\1', tocurl)
+        print(tocurl)
+
+#read TOC
+recs = []
+tocpage = BeautifulSoup(urllib.request.urlopen(tocurl, timeout=300), features="lxml")
+for a in tocpage.find_all('a'):
+    if a.has_attr('href') and re.search('abstract.html', a['href']):
+        rec = {'jnl' : 'Condensed Matter Phys.', 'vol' : vol, 'issue' : iss, 
+               'tc' : 'P', 'license' : {'statement' : 'CC-BY-4.0'},
+               'autaff' : []}
+        rec['artlink'] = tocurltrunc + a['href']
+        recs.append(rec)
+
+i = 0
+for rec in recs:
+    i += 1
+    ejlmod3.printprogress("-", [[i, len(recs)], [rec['artlink']]])
+    artpage = BeautifulSoup(urllib.request.urlopen(rec['artlink']), features="lxml")
+    time.sleep(2)
+    for a in artpage.find_all('a'):
+        if a.has_attr('href'):
+            #DOI
+            if re.search('doi.org/10', a['href']):
+                rec['doi'] = re.sub('.*doi.org\/', '', a['href'])
+                a.replace_with('YYY')
+            #fulltext
+            elif re.search('pdf$', a['href']):
+                rec['FFT'] = re.sub('(.*\/).*', r'\1', rec['artlink']) + a['href']
+                a.replace_with('ZZZ')        
+    #abstract
+    for bq in artpage.find_all('blockquote'):
+        rec['abs'] = re.sub('[\n\t\r]', ' ', bq.text.strip())
+        bq.replace_with('WWW')
+    #affiliations [mal formatted HTML]
+    #for font in artpage.find_all('font'):
+    #    ft = ''
+    #    for aff in re.split('; ', font.text.strip()):
+    #        aff = re.sub('^\(', '', aff)
+    #        aff = re.sub('\)$', '', aff)
+    #        ft += ' Aff=' + aff
+    #    font.replace_with(ft)
+    #work now on plain text
+    at = re.sub('[\n\t\r]', ' ', artpage.text)
+    #pubnote
+    rec['year'] = re.sub('.* ([12]\d\d\d),.*YYY.*', r'\1', at)
+    rec['p1'] = re.sub('.*, (\d+).*YYY.*', r'\1', at)
+    #arXiv
+    if re.search('arXiv:.*Title:', at):
+        rec['arxiv'] = re.sub('.*arXiv:(.*\d).*?Title:.*', r'\1', at)
+    #title
+    rec['tit'] = re.sub('.*?Title: *(.*) *Author.s.:.*', r'\1', at)
+    #keywords
+    if re.search('WWW.*Key words:', at):
+        rec['keyw'] = re.split(', ', re.sub('.*WWW.*Key words: (.*?) *(PACS|===|Full text).*', r'\1', at))
+    #PACS
+    if re.search('WWW.*PACS:', at):
+        rec['pacs'] = re.split(', ', re.sub('.*WWW.*PACS: (.*?) *(===|Full text).*', r'\1', at))
+
+    #authors
+    authors = re.sub('.*Author.s.: *(.*) *WWW.*', r'\1', at)
+    authors =  re.sub('^\&nbsp *', '', authors)
+    for author in re.split(' *, *\&nbsp *', authors):
+        if not re.search('Aff=', author):
+            author = re.sub('\((.*)', r' Aff=\1', author)
+            author = re.sub('\) *$', '', author)
+        rec['autaff'].append(re.split(' *Aff=', author))
+
+
+    ejlmod3.printrecsummary(rec)
+
+ejlmod3.writenewXML(recs, publisher, jnlfilename)
