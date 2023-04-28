9e1c23ca4035a622485ba36888cf801e01e2793a
Florian Schwennsen <florian.schwennsen@desy.de>
2023-04-28 18:52:02 +0200

    theses-texas3: use ejlmod3.getalreadyharvested()

--- active/theses-texas3.py
+++ active/theses-texas3.py
@@ -16,49 +16,56 @@ import time
 import json
 
 publisher = 'Texas U.'
+jnlfilename = 'THESES-TEXAS_U-%s' % (ejlmod3.stampoftoday())
 
 rpp = 50
+skipalreadyharvested = True
 
+
+if skipalreadyharvested:
+    alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
+    
 hdr = {'User-Agent' : 'Magic Browser'}
+recs = []    
 for department in ['Physics', 'Mathematics']:
     tocurl = 'https://repositories.lib.utexas.edu/handle/2152/4/browse?type=department&value=' + department + '&sort_by=2&order=DESC&rpp=' + str(rpp)
     ejlmod3.printprogress('=', [[department], [tocurl]])
     req = urllib.request.Request(tocurl, headers=hdr)
     tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-    recs = []    
-    recs = ejlmod3.getdspacerecs(tocpage, 'https://repositories.lib.utexas.edu/')
+    recs += ejlmod3.getdspacerecs(tocpage, 'https://repositories.lib.utexas.edu/', alreadyharvested=alreadyharvested)
     time.sleep(30)
 
-    i = 0
-    for rec in recs:
-        i += 1
-        ejlmod3.printprogress('-', [[department], [i, len(recs)], [rec['link']]])
+i = 0
+for rec in recs:
+    i += 1
+    ejlmod3.printprogress('-', [[department], [i, len(recs)], [rec['link']]])
+    try:
+        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
+        time.sleep(3)
+    except:
         try:
+            print("retry %s in 180 seconds" % (rec['link']))
+            time.sleep(180)
             artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-            time.sleep(3)
         except:
-            try:
-                print("retry %s in 180 seconds" % (rec['link']))
-                time.sleep(180)
-                artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-            except:
-                print("no access to %s" % (rec['link']))
-                continue
-        ejlmod3.metatagcheck(rec, artpage, ['DC.title', 'DCTERMS.issued', 'DC.subject', 'DCTERMS.abstract', 'citation_pdf_url', 'DC.identifier'])
-        if department == 'Mathematics':
-            rec['fc'] = 'm'
-        for meta in artpage.head.find_all('meta'):
-            if meta.has_attr('name'):
-                #author
-                if meta['name'] == 'DC.creator':
-                    if re.search('\d\d\d\d\-\d\d\d\d',  meta['content']):
-                        rec['autaff'][-1].append('ORCID:' + meta['content'])
-                    else:
-                        author = re.sub(' *\[.*', '', meta['content'])
-                        rec['autaff'] = [[ author ]]
-        rec['autaff'][-1].append(publisher)
-        ejlmod3.globallicensesearch(rec, artpage)
-        ejlmod3.printrecsummary(rec)
-    jnlfilename = 'THESES-TEXAS_U-%s_%s' % (ejlmod3.stampoftoday(), re.sub('\W', '', department))
+            print("no access to %s" % (rec['link']))
+            continue
+    ejlmod3.metatagcheck(rec, artpage, ['DC.title', 'DCTERMS.issued', 'DC.subject', 'DCTERMS.abstract', 'citation_pdf_url', 'DC.identifier'])
+    if department == 'Mathematics':
+        rec['fc'] = 'm'
+    for meta in artpage.head.find_all('meta'):
+        if meta.has_attr('name'):
+            #author
+            if meta['name'] == 'DC.creator':
+                if re.search('\d\d\d\d\-\d\d\d\d',  meta['content']):
+                    rec['autaff'][-1].append('ORCID:' + meta['content'])
+                else:
+                    author = re.sub(' *\[.*', '', meta['content'])
+                    rec['autaff'] = [[ author ]]
+    rec['autaff'][-1].append(publisher)
+    ejlmod3.globallicensesearch(rec, artpage)
+    ejlmod3.printrecsummary(rec)
 
-    ejlmod3.writenewXML(recs, publisher, jnlfilename)
+ejlmod3.writenewXML(recs, publisher, jnlfilename)
