56071eab29922551a31cc300803897ebb4ec2c10
Florian Schwennsen <florian.schwennsen@desy.de>
2023-04-28 18:51:41 +0200

    theses-oslo3: use ejlmod3.getalreadyharvested()

--- active/theses-oslo3.py
+++ active/theses-oslo3.py
@@ -13,28 +13,26 @@ publisher = 'Oslo U.'
 rpp = 20
 numofpages = 1
 jnlfilename = 'THESES-OSLO-%s' % (ejlmod3.stampoftoday())
+skipalreadyharvested = True
 
+if skipalreadyharvested:
+    alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
 
 hdr = {'User-Agent' : 'Magic Browser'}
 recs = []
-for (depnr, dep) in [(14, 'Phys'), (11, 'Astro'), (6, 'Math')]:
+for (depnr, fc) in [(14, ''), (11, 'a'), (6, 'm'), ('3', 'c')]:
     for i in range(numofpages):
         tocurl = 'https://www.duo.uio.no/handle/10852/' + str(depnr) + '/discover?order=DESC&rpp=' + str(rpp) + '&sort_by=dc.date.issued_dt&page=' + str(i+1) + '&group_by=none&etal=0&filtertype_0=type&filter_0=Doktoravhandling&filter_relational_operator_0=equals'
-        ejlmod3.printprogress('=', [[depnr, dep], [i+1, numofpages], [tocurl]])
+        ejlmod3.printprogress('=', [[depnr, fc], [i+1, numofpages], [tocurl]])
         req = urllib.request.Request(tocurl, headers=hdr)
         tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
         time.sleep(2)
-        for div in tocpage.body.find_all('div', attrs = {'class' : 'artifact-title'}):
-            for a in div.find_all('a'):
-                rec = {'tc' : 'T', 'jnl' : 'BOOK', 'note' : [], 'autaff' : []}
-                rec['link'] = 'https://www.duo.uio.no' + a['href']
-                rec['hdl'] = re.sub('\/handle\/', '', a['href'])
-                rec['tit'] = a.text.strip()
-                if dep == 'Astro':
-                    rec['fc'] = 'a'
-                elif dep == 'Math':
-                    rec['fc'] = 'm'
-                recs.append(rec)
+        for rec in ejlmod3.getdspacerecs(tocpage, 'https://www.duo.uio.no', alreadyharvested=alreadyharvested):
+            if fc:
+                rec['fc'] = fc
+            recs.append(rec)
             
 j = 0
 for rec in recs:
