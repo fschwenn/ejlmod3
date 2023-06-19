66995ad090d1c07ca99266b72f313cf400d57884
Florian Schwennsen <florian.schwennsen@desy.de>
2023-06-19 10:59:09 +0200

    baos3: added 'Bulg.Astron.J.'

--- active/baos3.py
+++ active/baos3.py
@@ -11,26 +11,31 @@ import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 import time
 
-
-
+jnls = {'cr' : 'Compt.Rend.Acad.Bulg.Sci.',
+        'aj' : 'Bulg.Astron.J.'}
 
 regexpref = re.compile('[\n\r\t]')
 
-typecode = 'P'
-
-issueid = sys.argv[1]
+jnl = sys.argv[1]
 
 publisher = 'Bulgarian Academy of Sciences'
-urltrunk = 'http://www.proceedings.bas.bg/index.php/cr/issue/view/%s' % (issueid)
+
+
+if jnl == 'cr':
+    issueid = sys.argv[2]
+    urltrunc = 'http://www.proceedings.bas.bg/index.php/cr/issue/view/%s' % (issueid)
+elif jnl == 'aj':
+    volume = sys.argv[2]
+    urltrunc = 'https://astro.bas.bg/AIJ/issues/n' + volume
     
-print(urltrunk)
+print(urltrunc)
 try:
-    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(urltrunk), features="lxml")
+    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(urltrunc), features="lxml")
     time.sleep(3)
 except:
-    print("retry %s in 180 seconds" % (urltrunk))
+    print("retry %s in 180 seconds" % (urltrunc))
     time.sleep(180)
-    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(urltrunk), features="lxml")
+    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(urltrunc), features="lxml")
 
 recs = []
 for div in tocpage.body.find_all('div', attrs = {'class' : 'section'}):
@@ -38,8 +43,7 @@ for div in tocpage.body.find_all('div', attrs = {'class' : 'section'}):
         note = regexpref.sub('', h2.text).strip()
         print(' - %s - ' % (note))
     for h3 in div.find_all('h3'):
-        rec = {'jnl' : 'Compt.Rend.Acad.Bulg.Sci.', 'tc' : 'P', 'note' : [note], 'keyw' : [],
-               'autaff' : []}
+        rec = {'jnl' : jnls[jnl], 'tc' : 'P', 'note' : [note], 'keyw' : [], 'autaff' : []}
         if note in ['Mathematics']:
             rec['fc'] = 'm'
         elif note in ['Chemistry', 'Biology', 'Medicine', 'Geophysics', 'Agricultural Sciences'
@@ -65,5 +69,25 @@ for rec in recs:
                                         'citation_reference'])
     ejlmod3.printrecsummary(rec)
 
-jnlfilename = 'crabs%s.%s_%s' % (rec['vol'], rec['issue'], issueid)
+if not recs:
+    for h1 in tocpage.body.find_all('h1'):
+        if re.search('\([12]\d\d\d\)', h1.text):
+            year = re.sub('.*\(([12]\d\d\d)\).*', r'\1', h1.text.strip())
+    for div in tocpage.body.find_all('div', attrs = {'class' : 'media'}):
+        rec = {'jnl' : jnls[jnl], 'tc' : 'P', 'vol' : volume, 'year' : year}
+        for a in div.find_all('a'):
+            rec['FFT'] = '%s/%s' % (urltrunc, a['href'])
+            at = a.text.strip()
+            if at:
+                rec['tit'] = at
+            a.decompose()
+        rec['auts'] = re.split(' *, *', re.sub(' and ', ', ', regexpref.sub('', div.text.strip())))
+        rec['doi'] = '30.3000/bas.bg/%s/%s/%s' % (jnl, volume, re.sub('\W', '', rec['tit']))
+        recs.append(rec)
+        ejlmod3.printrecsummary(rec)
+    
+if jnl == 'cr':
+    jnlfilename = 'crabs%s.%s_%s' % (rec['vol'], rec['issue'], issueid)
+elif jnl == 'aj':
+    jnlfilename = 'bulgaj%s_%s' % (volume, ejlmod3.stampoftoday())
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
