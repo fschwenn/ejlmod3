d4ec0cc12b449f6b0fe5a44fb3209016c4c085c2
Florian Schwennsen <florian.schwennsen@desy.de>
2023-09-20 15:02:59 +0200

    osa3: updated chrome-setting

--- active/osa3.py
+++ active/osa3.py
@@ -53,7 +53,7 @@ options = uc.ChromeOptions()
 #options.binary_location='/usr/bin/chromium-browser'
 #options.binary_location='/afs/desy.de/user/l/library/tmp/chromedriver109.0.5414.74'
 options.binary_location='/usr/bin/google-chrome'
-#options.binary_location='/usr/bin/chromium-browser'
+options.binary_location='/usr/bin/chromium'
 options.add_argument('--headless')
 options.add_argument("--no-sandbox")
 #options.add_argument("--incognito")
