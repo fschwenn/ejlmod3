19d8a81bd16b9e9583c49852e314c9473497861d
Florian Schwennsen <florian.schwennsen@desy.de>
2023-09-20 15:02:34 +0200

    oxfordbooks3: use undetected chrome driver, slight changes in web layout

--- active/oxfordbooks3.py
+++ active/oxfordbooks3.py
@@ -11,6 +11,7 @@ import urllib.request, urllib.error, urllib.parse
 import urllib.parse
 import time
 from bs4 import BeautifulSoup
+import undetected_chromedriver as uc
 
 
 publisher = 'Oxford University Press'
@@ -31,6 +32,15 @@ serieses = ['physics/astronomy-and-astrophysics',
 facetsandsort = '?cc=de&lang=en&prevSortField=8&facet_narrowbytype_facet=Academic+Research&facet_narrowbytype_facet=Books+for+Courses&sortField=8&resultsPerPage=20&start=0'
 
 
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+options.add_argument("--enable-javascript")
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
+
+driver.get('https://global.oup.com/academic/')
+time.sleep(180)
+
 #scan serieses
 isbnsdone = []
 prerecs = []
@@ -38,15 +48,20 @@ for series in serieses:
     toclink = '%s/%s/%s' % (urltrunc, series, facetsandsort)
     subject = re.sub('.*\/', '', series)
     ejlmod3.printprogress('=', [ [series], [toclink] ])
-    tocreq = urllib.request.Request(toclink, headers={'User-Agent' : "Magic Browser"}) 
-    toc = BeautifulSoup(urllib.request.urlopen(tocreq), features="lxml")
-    for td in toc.body.find_all('td', attrs = {'class' : 'result_biblio'}):        
+    #tocreq = urllib.request.Request(toclink, headers={'User-Agent' : "Magic Browser"}) 
+    #toc = BeautifulSoup(urllib.request.urlopen(tocreq), features="lxml")
+    driver.get(toclink)
+    toc = BeautifulSoup(driver.page_source, features="lxml")
+    #print(toc.text)
+    for td in toc.body.find_all('td', attrs = {'class' : 'result_biblio'}):
         for h2 in td.find_all('h2'):
             for a in h2.find_all('a'):
                 artlink = 'https://global.oup.com' + a['href']
                 rec = {'tit' : a.text.strip(), 'artlink' : artlink, 'note' : [ subject ],
                        'tc' : 'B', 'jnl' : 'BOOK', }
+#                if not artlink in ['https://global.oup.com/academic/product/quantum-fields--from-the-hubble-to-the-planck-scale-9780192873491?prevSortField=8&facet_narrowbytype_facet=Academic%20Research&sortField=8&resultsPerPage=20&start=0&lang=en&cc=de']:
                 prerecs.append(rec)
+    print('  %4i records so far' % (len(prerecs)))
     time.sleep(130)
 
 #scan individual book pages
@@ -55,8 +70,10 @@ recs = []
 for rec in prerecs:        
     i += 1
     ejlmod3.printprogress('-', [[i, len(prerecs)], [rec['artlink']], [len(recs)]])
-    artreq = urllib.request.Request(rec['artlink'], headers={'User-Agent' : "Magic Browser"}) 
-    art = BeautifulSoup(urllib.request.urlopen(artreq), features="lxml")
+    #artreq = urllib.request.Request(rec['artlink'], headers={'User-Agent' : "Magic Browser"}) 
+    #art = BeautifulSoup(urllib.request.urlopen(artreq), features="lxml")
+    driver.get(rec['artlink'])
+    art = BeautifulSoup(driver.page_source, features="lxml")
     #abstract and ISBN
     for meta in art.head.find_all('meta'):
         if meta.has_attr('property'):
@@ -94,18 +111,19 @@ for rec in prerecs:
     time.sleep(20)
     addrecord = True
     #check if too new
-    if re.search('Estimated', rec['date']):
-        #print('   [%2i/%2i] delete "%s" because date=%s' % (i, len(prerecs), rec['tit'], rec['date']))
+    if 'date' in rec and re.search('Estimated', rec['date']):
+        print('   [%2i/%2i] delete "%s" because date=%s' % (i, len(prerecs), rec['tit'], rec['date']))
         addrecord = False
         continue
     #check if already in other subject
-    for isbn in rec['isbns']:
-        if isbn in isbnsdone:
-            #print('   [%2i/%2i] delete "%s" because already under other subject or with other ISBN' % (i, len(prerecs), rec['tit']))
-            addrecord = False
-            continue
-        else:
-            isbnsdone.append(isbn)
+    if 'isbns' in rec:
+        for isbn in rec['isbns']:
+            if isbn in isbnsdone:
+                print('   [%2i/%2i] delete "%s" because already under other subject or with other ISBN' % (i, len(prerecs), rec['tit']))
+                addrecord = False
+                continue
+            else:
+                isbnsdone.append(isbn)
     if addrecord:
         #print('   [%2i/%2i] added "%s"' % (i, len(prerecs), rec['tit']))
         recs.append(rec)
