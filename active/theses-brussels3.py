e203f5926a59992f8ec0820d6a6f58ad3456e263
Florian Schwennsen <florian.schwennsen@desy.de>
2023-09-23 10:27:06 +0200

    theses-brussels3: updated chrome setting

--- active/theses-brussels3.py
+++ active/theses-brussels3.py
@@ -72,6 +72,7 @@ options = uc.ChromeOptions()
 options.add_argument('--headless')
 options.binary_location='/usr/bin/chromium-browser'
 options.binary_location='/usr/bin/google-chrome'
+options.binary_location='/usr/bin/chromium'
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
 
