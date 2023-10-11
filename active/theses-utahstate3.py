cd00869e5e3ccbde22e9f0a770a1f24e23f70e1b
Florian Schwennsen <florian.schwennsen@desy.de>
2023-10-11 12:34:48 +0200

    theses-utahstate3: major rewrite

--- active/theses-utahstate3.py
+++ active/theses-utahstate3.py
@@ -16,7 +16,7 @@ import os
 publisher = 'Utah State U.'
 jnlfilename = 'THESES-UTAHSTATE-%s' % (ejlmod3.stampoftoday())
 rpp = 25
-pages = 1000
+pages = 1000-950
 skipalreadyharvested = True
 
 startyear = ejlmod3.year(backwards=1)
@@ -26,8 +26,9 @@ boring += ['Special Education and Rehabilitation Counseling', 'Psychology', 'Bio
 
 # Initiate webdriver
 options = uc.ChromeOptions()
-options.add_argument('--headless')
+#options.add_argument('--headless')
 options.binary_location='/usr/bin/google-chrome'
+options.binary_location='/usr/bin/chromium'
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
 
@@ -35,10 +36,11 @@ if skipalreadyharvested:
     alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
 
 recs = []
+prerecs = []
 complete = False
 for page in range(pages):
     tocurl = 'https://digitalcommons.usu.edu/do/search/?q=document_type%3Athesis&start=' + str(rpp*page) + '&start_date=' + str(startyear) + '-01-01&end_date=' + str(stopyear) + '-12-31&context=656526&sort=date_desc&facet=#query-results'
-    ejlmod3.printprogress('=', [[page+1, pages], [tocurl]])
+    ejlmod3.printprogress('=', [[page+1, pages], [tocurl], [len(prerecs)]])
     if page == 0:
         driver.get(tocurl)
         WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'title')))
@@ -74,57 +76,58 @@ for page in range(pages):
             continue
         else:
             print("[{}] --> Harvesting data".format(rec['artlink']))
+            prerecs.append(rec)
+    sleep(5)
 
-        driver.get(rec['artlink'])
+for (i, rec) in enumerate(prerecs):
+    ejlmod3.printprogress('-', [[i+1, len(prerecs)], [rec['artlink']], [len(recs)]])
+    driver.get(rec['artlink'])
 
-        article_soup = BeautifulSoup(driver.page_source, 'lxml')
-        ejlmod3.metatagcheck(rec, article_soup, ['bepress_citation_author',
-           'bepress_citation_title', 'bepress_citation_date', 'keywords', 'keywords', 'description'])
-
-        # Get the degree
-        degree_section = article_soup.find_all('div', attrs={'id': 'degree_name'})
-        if len(degree_section) == 1:
-            if degree_section[0].find_all('p')[0].text in boring:
-                print("[{}] --> Skipping because of boring degree".format(article_link[0].get('href')))
-                keepit = False
-            else:
-                rec['note'].append(degree_section[0].find_all('p')[0].text)
-
-        # Get the department
-        department_section = article_soup.find_all('div', attrs={'id': 'department'})
-        if len(department_section) == 1:
-            if department_section[0].find_all('p')[0].text in boring:
-                print("[{}] --> Skipping because of boring department".format(article_link[0].get('href')))
-                keepit = False
-            else:
-                rec['note'].append(department_section[0].find_all('p')[0].text)
-
-        # Get the committee
-        committee_section = article_soup.find_all('div', attrs={'id': 'committee_chair'})
-        if len(committee_section) == 1:
-            rec['supervisor'] = [[committee_section[0].find_all('p')[0].text]]
-
-        # Get the DOI
-        doi_section = article_soup.find_all('div', attrs={'id': 'doi'})
-        if len(doi_section) == 1:
-            doi_link = doi_section[0].find_all('a')[0].text
-
-            rec['doi'] = doi_link.replace('https://doi.org/', '')
-
-        # Get the PDF link
-        pdf_link = article_soup.find_all('a', attrs={'id': 'pdf', 'class': 'btn'})
-        if len(pdf_link) == 1:
-            rec['hidden'] = pdf_link[0].get('href')
-
-        if keepit:
-            if not skipalreadyharvested or not rec['doi'] in alreadyharvested:
-                recs.append(rec)
-                ejlmod3.printrecsummary(rec)
+    article_soup = BeautifulSoup(driver.page_source, 'lxml')
+    ejlmod3.metatagcheck(rec, article_soup, ['bepress_citation_author', 'bepress_citation_title',
+                                             'bepress_citation_date', 'keywords', 'keywords',
+                                             'description'])
+    # Get the degree
+    degree_section = article_soup.find_all('div', attrs={'id': 'degree_name'})
+    if len(degree_section) == 1:
+        if degree_section[0].find_all('p')[0].text in boring:
+            print("[{}] --> Skipping because of boring degree".format(article_link[0].get('href')))
+            keepit = False
         else:
-            ejlmod3.adduninterestingDOI(rec['doi'])
+            rec['note'].append(degree_section[0].find_all('p')[0].text)
 
-        sleep(5)
-    print('  %4i records so far' % (len(recs)))
+    # Get the department
+    department_section = article_soup.find_all('div', attrs={'id': 'department'})
+    if len(department_section) == 1:
+        if department_section[0].find_all('p')[0].text in boring:
+            print("[{}] --> Skipping because of boring department".format(article_link[0].get('href')))
+            keepit = False
+        else:
+            rec['note'].append(department_section[0].find_all('p')[0].text)
+
+    # Get the committee
+    committee_section = article_soup.find_all('div', attrs={'id': 'committee_chair'})
+    if len(committee_section) == 1:
+        rec['supervisor'] = [[committee_section[0].find_all('p')[0].text]]
+
+    # Get the DOI
+    doi_section = article_soup.find_all('div', attrs={'id': 'doi'})
+    if len(doi_section) == 1:
+        doi_link = doi_section[0].find_all('a')[0].text
+    rec['doi'] = doi_link.replace('https://doi.org/', '')
+
+    # Get the PDF link
+    pdf_link = article_soup.find_all('a', attrs={'id': 'pdf', 'class': 'btn'})
+    if len(pdf_link) == 1:
+        rec['hidden'] = pdf_link[0].get('href')
+
+    if keepit:
+        if not skipalreadyharvested or not rec['doi'] in alreadyharvested:
+            recs.append(rec)
+            ejlmod3.printrecsummary(rec)
+    else:
+        ejlmod3.adduninterestingDOI(rec['doi'])
+
     sleep(5)
 
 driver.close()
