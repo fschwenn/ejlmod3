5b70c35b50722b7524ce0b7636617246ae737542
Florian Schwennsen <florian.schwennsen@desy.de>
2023-10-11 12:33:28 +0200

    theses-patras3: updated chrome-setting

--- active/theses-patras3.py
+++ active/theses-patras3.py
@@ -22,6 +22,8 @@ skipalreadyharvested = True
 
 options = uc.ChromeOptions()
 options.binary_location='/usr/bin/google-chrome'
+options.binary_location='/usr/bin/chromium'
+
 options.add_argument('--headless')
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
