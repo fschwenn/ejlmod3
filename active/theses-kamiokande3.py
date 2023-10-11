0fa8158a6bb5e265e2e479e29821c8fe890a4866
Florian Schwennsen <florian.schwennsen@desy.de>
2023-10-11 12:35:16 +0200

    theses-kamiokande3: major rewrite

--- active/theses-kamiokande3.py
+++ active/theses-kamiokande3.py
@@ -22,6 +22,7 @@ recs = []
 
 #server1
 tocurl = 'http://www-sk.icrr.u-tokyo.ac.jp/sk/publications/index-e.html'
+tocurl = 'https://www-sk.icrr.u-tokyo.ac.jp/en/sk/for-reseacher/'
 print(tocurl)
 
 if skipalreadyharvested:    
@@ -35,37 +36,48 @@ try:
 except:
     print("retry %s in 180 seconds" % (tocurl))
     time.sleep(180)
-    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl))
+    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl), features="lxml")
 
-for p in tocpage.find_all('p'):
-    pt = re.sub('[\n\t\r]', ' ', p.text.strip())
-    if re.search('PhD', pt):
-        rec = {'tc' : 'T', 'jnl' : 'BOOK'}
-        for br in p.find_all('br'):
-            br.replace_with(', ')
-        for a in p.find_all('a'):
-            if re.search('pdf$', a['href']):
-                rec['link'] = 'http://www-sk.icrr.u-tokyo.ac.jp/sk' + a['href'][2:]
-                rec['hidden'] = 'http://www-sk.icrr.u-tokyo.ac.jp/sk' + a['href'][2:]
-                rec['doi'] = '20.2000/KAMIOKANDE/' + re.sub('\W', '', a['href'][16:-4])
-            a.decompose()
-        pt = re.sub('[\n\t\r]', ' ', p.text.strip())
-        #print pt
-        halfs = re.split(' *,? *PhD Thesis *,? *', pt)
-        firstparts = re.split(' *, *', halfs[0])
-        secondparts = re.split(' *, *', halfs[1])
-        #print 'SP', secondparts
-        if re.search('\d', secondparts[-1]):
-            rec['date'] = re.sub('.*([12]\d\d\d).*', r'\1', secondparts[-1])
-            rec['year'] = rec['date']
-            rec['autaff'] = [[ firstparts[-1], ', '.join(secondparts[:-1]) ]]
-        else:
-            rec['date'] = re.sub('.*([12]\d\d\d).*', r'\1', secondparts[-2])
-            rec['year'] = rec['date']
-            rec['autaff'] = [[ firstparts[-1], ', '.join(secondparts[:-2]) ]]
-        rec['tit'] = ', '.join(firstparts[:-1])
-        if int(rec['year']) >= ejlmod3.year(backwards=2):
-            if not skipalreadyharvested or not rec['doi'] in alreadyharvested:
-                recs.append(rec)
+for div in tocpage.find_all('div', attrs = {'id' : 'doctor'}):
+    for ul in div.find_all('li', attrs = {'class' : 'paperList__thesisList__item'}):
+        year = False
+        keepit = True
+        for p in ul.find_all('p', attrs = {'class' : 'paperList__year'}):
+            pt = re.sub('[\n\t\r]', ' ', p.text.strip())
+            if re.search('^\d+$', pt):
+                year = int(pt)
+                if year <= ejlmod3.year(backwards=years):
+                    print(year, 'too old')
+                    keepit = False
+        if keepit:
+            for li in ul.find_all('li', attrs = {'class' : 'list__dotList__item'}):
+                rec = {'tc' : 'T', 'jnl' : 'BOOK'}
+                if year:
+                    rec['year'] = year
+                for a in li.find_all('a'):
+                    if re.search('pdf$', a['href']):
+                        rec['link'] = 'http://www-sk.icrr.u-tokyo.ac.jp' + a['href']
+                        rec['hidden'] = 'http://www-sk.icrr.u-tokyo.ac.jp' + a['href']
+                        rec['doi'] = '20.2000/KAMIOKANDE/' + re.sub('\W', '', a['href'][17:-4])
+                    a.decompose()
+                for p in li.find_all('p', attrs = {'class' : 'paperList__read'}):
+                    pt = re.sub('[\n\t\r]', ' ', p.text.strip())
+                    #print pt
+                    halfs = re.split(' *,? *PhD Thesis *,? *', pt)
+                    firstparts = re.split(' *, *', halfs[0])
+                    secondparts = re.split(' *, *', halfs[1])
+                    #print 'SP', secondparts
+                    if re.search('\d', secondparts[-1]):
+                        rec['date'] = re.sub('.*([12]\d\d\d).*', r'\1', secondparts[-1])
+                        rec['year'] = rec['date']
+                        rec['autaff'] = [[ firstparts[-1], ', '.join(secondparts[:-1]) ]]
+                    else:
+                        rec['date'] = re.sub('.*([12]\d\d\d).*', r'\1', secondparts[-2])
+                        rec['year'] = rec['date']
+                        rec['autaff'] = [[ firstparts[-1], ', '.join(secondparts[:-2]) ]]
+                    rec['tit'] = ', '.join(firstparts[:-1])
+                if not skipalreadyharvested or not rec['doi'] in alreadyharvested:
+                    recs.append(rec)
+                    ejlmod3.printrecsummary(rec)
 
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
