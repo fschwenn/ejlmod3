1bb72f8f83473dde034cdcc8081bfce96ebc3050
Florian Schwennsen <florian.schwennsen@desy.de>
2023-10-17 14:11:16 +0200

    theses-iisc3:  updated chrome setting

--- active/theses-iisc3.py
+++ active/theses-iisc3.py
@@ -58,6 +58,7 @@ for (section, fc) in sections:
 
 options = uc.ChromeOptions()
 options.binary_location='/opt/google/chrome/google-chrome'
+options.binary_location='/usr/bin/chromium'
 options.add_argument('--headless')
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
