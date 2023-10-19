94f2aefbca094b2a0db0a1ea9e9ed8b23298507e
Florian Schwennsen <florian.schwennsen@desy.de>
2023-10-19 18:48:28 +0200

    mdpi.sftp3: remove credentials

--- active/mdpi.sftp3.py
+++ active/mdpi.sftp3.py
@@ -161,7 +161,7 @@ if not os.path.isdir(tmppath):
 print('connect to ftp://download.mdpi.com') 
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
-srv = pysftp.Connection(host="download.mdpi.com", username="mdpi_public_ftp", password="XXXXXXXXXXXx", port=9922, cnopts=cnopts)
+srv = pysftp.Connection(host="XXX", username="XXX", password="XXX")
 srv.cwd('MDPI_corpus')
 srv.cwd(jnl)
 issueslocal = []
