00383e431852f3a6314da771a5a799395f35cc81
Florian Schwennsen <florian.schwennsen@desy.de>
2023-11-29 08:52:03 +0100

    theses-texas3: DSpace update => changed to ngrx

--- active/theses-texas3.py
+++ active/theses-texas3.py
@@ -2,6 +2,7 @@
 #harvest theses from Texas U.
 #FS: 2019-12-09
 #FS: 2022-09-10
+#FS: 2023-11-28
 
 import getopt
 import sys
@@ -14,12 +15,40 @@ import codecs
 import datetime
 import time
 import json
+import undetected_chromedriver as uc
 
 publisher = 'Texas U.'
 jnlfilename = 'THESES-TEXAS_U-%s' % (ejlmod3.stampoftoday())
 
-rpp = 50
+rpp = 100
+pages = 10
 skipalreadyharvested = True
+
+boring = ['Cello performance', 'Chemistry', 'Civil Engineering', 'Clarinet performance',
+          'Collaborative piano', 'Composition', 'Mechanical Engineering', 'Percussion performance',
+          'Petroleum Engineering', 'Physics', 'Piano perfomance', 'Piano performance',
+          'Psychology - Clinical Psychology', 'Psychology', 'Social work', 'Sociology',
+          'Trombone performance', 'Trumpet performance', 'Vocal performance', 'Public Affairs',
+          'Music', 'Economics', 'Advertising', 'Architecture', 'Biomedical Engineering',
+          'Cell and Molecular Biology', 'Chemical Engineering', 'Communication Studies',
+          'Educational Leadership and Policy', 'Geological Sciences', 'Government', 'History',
+          'Linguistics', 'Public Policy (PhD)', 'Special Education', 'Music and Human Learning',
+          'Speech, Language, and Hearing Sciences', 'Accounting', 'Aerospace Engineering',
+          'Anthropology', 'Bassoon performance', 'Community and Regional Planning',
+          'Comparative literature', 'Curriculum and Instruction', 'Ecology, Evolution, and Behavior',
+          'Educational leadership and policy', 'Educational Psychology - School', 'Plant Biology', 
+          'Educational Psychology', 'Engineering Mechanics', 'Microbiology', 'Philosophy',
+          'Environmental and Water Resources Engineering', 'Finance', 'French', 'Germanic Studies',
+          'Kinesiology', 'Management', 'Marketing', 'Materials Science and Engineering',
+          'Nursing', 'Operations Research and Industrial Engineering', 'Pharmaceutical Sciences',
+          'Radio-Television-Film',  'Social Work', 'Speech Language Pathology', 'Violin performance',
+          'Science, Technology, Engineering, and Mathematics (STEM) Education', 'American Studies',
+          'Biochemistry', 'Educational Psychology - Counseling', 'English', 'Geography',
+          'Health Behavior and Health Education', 'Human Development and Family Sciences',
+          'Latin American Studies', 'Marine science', 'Neuroscience', 'Nutritional Sciences',
+          'Spanish', 'African and African Diaspora Studies', 'Architectural Engineering',
+          'Information, Risk, and Operations Management', 'Information Studies']
+boring += ['Doctor of Education', 'Doctor of Musical Arts', 'Artist diploma', 'Master of Music']
 
 
 if skipalreadyharvested:
@@ -28,44 +57,47 @@ else:
     alreadyharvested = []
     
 hdr = {'User-Agent' : 'Magic Browser'}
-recs = []    
-for department in ['Physics', 'Mathematics']:
-    tocurl = 'https://repositories.lib.utexas.edu/handle/2152/4/browse?type=department&value=' + department + '&sort_by=2&order=DESC&rpp=' + str(rpp)
-    ejlmod3.printprogress('=', [[department], [tocurl]])
-    req = urllib.request.Request(tocurl, headers=hdr)
-    tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-    recs += ejlmod3.getdspacerecs(tocpage, 'https://repositories.lib.utexas.edu/', alreadyharvested=alreadyharvested)
-    time.sleep(30)
-
-i = 0
-for rec in recs:
-    i += 1
-    ejlmod3.printprogress('-', [[department], [i, len(recs)], [rec['link']]])
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
+driver.get('https://utexas.edu')
+time.sleep(10)
+driver.get('https://repositories.lib.utexas.edu')
+time.sleep(10)
+recs = []
+for page in range(pages):
+    tocurl = 'https://repositories.lib.utexas.edu/browse/dateissued?scope=4d334651-b63a-4e48-851c-938cbe4833ba&bbm.page=' + str(page+1) + '&bbm.sd=DESC&bbm.rpp=' + str(rpp)
+    ejlmod3.printprogress('=', [[page+1, pages], [tocurl]])
+    #req = urllib.request.Request(tocurl, headers=hdr)
+    #tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
     try:
-        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-        time.sleep(3)
+        driver.get(tocurl)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
     except:
-        try:
-            print("retry %s in 180 seconds" % (rec['link']))
-            time.sleep(180)
-            artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-        except:
-            print("no access to %s" % (rec['link']))
-            continue
-    ejlmod3.metatagcheck(rec, artpage, ['DC.title', 'DCTERMS.issued', 'DC.subject', 'DCTERMS.abstract', 'citation_pdf_url', 'DC.identifier'])
-    if department == 'Mathematics':
-        rec['fc'] = 'm'
-    for meta in artpage.head.find_all('meta'):
-        if meta.has_attr('name'):
-            #author
-            if meta['name'] == 'DC.creator':
-                if re.search('\d\d\d\d\-\d\d\d\d',  meta['content']):
-                    rec['autaff'][-1].append('ORCID:' + meta['content'])
-                else:
-                    author = re.sub(' *\[.*', '', meta['content'])
-                    rec['autaff'] = [[ author ]]
-    rec['autaff'][-1].append(publisher)
-    ejlmod3.globallicensesearch(rec, artpage)
-    ejlmod3.printrecsummary(rec)
+        time.sleep(60)
+        driver.get(tocurl)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    baseurl = 'https://repositories.lib.utexas.edu/'
+    for rec in ejlmod3.ngrx(tocpage, baseurl, ['dc.contributor.advisor', 'dc.creator', 'dc.creator.orcid',
+                                               'dc.date.issued', 'dc.description.abstract',
+                                               'dc.identifier', 'dc.language.iso',
+                                               'dc.rights', 'dc.title', 'dc.identifier.uri',
+                                               'thesis.degree.discipline',
+                                               'thesis.degree.level', 'thesis.degree.name'], boring=boring):
+        keepit = True
+
+        if keepit:
+            if skipalreadyharvested and 'doi' in rec and rec['doi'] in alreadyharvested:
+                print('    %s already in backup' % (rec['doi']))
+            elif skipalreadyharvested and 'hdl' in rec and rec['hdl'] in alreadyharvested:
+                print('    %s already in backup' % (rec['hdl']))
+            else:
+                rec['autaff'][-1].append(publisher)
+                ejlmod3.printrecsummary(rec)
+                recs.append(rec)
+        
+    time.sleep(20)
+
 
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
