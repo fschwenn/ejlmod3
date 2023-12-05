47779d18589d5862199bf7af4cbd0febe8b0253f
Florian Schwennsen <florian.schwennsen@desy.de>
2023-12-05 09:31:55 +0100

    theses-stellenbosch3: new DSpace-Version -> use  ejlmod3.ngrx()

--- active/theses-stellenbosch3.py
+++ active/theses-stellenbosch3.py
@@ -1,76 +1,61 @@
 # -*- coding: utf-8 -*-
 #harvest theses from Stellenbosch U.
 #FS: 2022-11-08
+#FS: 2023-12-02
 
-import urllib.request, urllib.error, urllib.parse
-import urllib.parse
 from bs4 import BeautifulSoup
 import re
 import ejlmod3
 import time
+import undetected_chromedriver as uc
+import os
 
 jnlfilename = 'THESES-STELLENBOSCH-%s' % (ejlmod3.stampoftoday())
 
 publisher = 'U. Stellenbosch'
 
-hdr = {'User-Agent' : 'Magic Browser'}
-
+skipalreadyharvested = True
 rpp = 10
 pages = 1
-years = 2
+boring = []
+
+
+if skipalreadyharvested:
+    alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
+
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
 
 recs = []
-for (fc, dep) in [('c', '96338'), ('m', '852'), ('', '3640'), ('c', '96343')]:
+for (fc, dep) in [('m', '6fa3027a-cbc2-420a-89b2-d14704b58f0e'),
+                  ('m', 'a0564044-0e05-451d-94ed-5fc5fd3160f4'),
+                  ('c', '6162c8d4-f5ce-45f1-ba42-225b8d7f9fb4'),
+                  ('', '628e526d-ce10-4841-a09b-48b265bd6412')]:
     for page in range(pages):
-        tocurl = 'https://scholar.sun.ac.za/handle/10019.1/' + dep + '/browse?rpp=' + str(rpp) + '&offset=' + str(rpp*page) + '&etal=-1&sort_by=2&order=DESC'
-        ejlmod3.printprogress('=', [[dep], [page+1, pages], [tocurl]])
-        req = urllib.request.Request(tocurl, headers=hdr)
-        tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-        prerecs = ejlmod3.getdspacerecs(tocpage, 'https://scholar.sun.ac.za', fakehdl=True)
-        for rec in prerecs:
-            keepit = True
-            #check year
-            if 'year' in rec and int(rec['year']) <= ejlmod3.year(backwards=years):
-                keepit = False
-            if keepit:
-                if fc: rec['fc'] = fc
-                recs.append(rec)
-    print('  %i records so far' % (len(recs)))
-    time.sleep(3)
-
-i = 0
-for rec in recs:
-    i += 1
-    ejlmod3.printprogress('-', [[i, len(recs)], [rec['link']]])
-    try:
-        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']+'?show=full'), features="lxml")
-        time.sleep(4)
-    except:
+        tocurl = 'https://scholar.sun.ac.za/collections/' + dep + '?cp.page=' + str(page+1) + '&cp.rpp=' + str(rpp)
+        ejlmod3.printprogress('=', [[fc,dep], [page+1, pages], [tocurl]])
         try:
-            print("retry %s in 180 seconds" % (rec['artlink']))
-            time.sleep(180)
-            artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']+'?show=full'), features="lxml")
+            driver.get(tocurl)
+            time.sleep(5)
+            tocpage = BeautifulSoup(driver.page_source, features="lxml")
         except:
-            print("no access to %s" % (rec['artlink']))
-            continue
-    ejlmod3.metatagcheck(rec, artpage, ['citation_title', 'citation_date', 'DCTERMS.abstract', 'citation_pdf_url',
-                                        'citation_language', 'citation_author', 'DC.rights', 'DC.subject', 'DCTERMS.extent'])
-    if 'abs' in rec:
-        rec['abs'] = re.sub('^ENGLISH[ A-Z]+:  *', '', rec['abs'])
-    for tr in artpage.body.find_all('tr'):
-        tdt = ''
-        for td in tr.find_all('td', attrs = {'class' : 'label-cell'}):
-            tdt = td.text.strip()
-            td.decompose()
-        for td in tr.find_all('td'):
-            if td.text.strip() in ['en_ZA', 'af_ZA', '']:
-                continue
-            #supervisor
-            if tdt in ['dc.contributor.supervisor', 'dc.contributor.advisor']:
-                if td.text.strip():
-                    rec['supervisor'].append([ re.sub(' \(.*', '', td.text.strip()) ])
-    rec['autaff'][-1].append(publisher)
-    ejlmod3.printrecsummary(rec)
-
+            time.sleep(60)
+            driver.get(tocurl)
+            time.sleep(5)
+            tocpage = BeautifulSoup(driver.page_source, features="lxml")
+        baseurl = 'https://scholar.sun.ac.za/'
+        
+        for rec in ejlmod3.ngrx(tocpage, baseurl, ['dc.contributor.advisor', 'dc.contributor.author', 'dc.date.issued', 'dc.description.abstract', 'dc.format.extent', 'dc.identifier.uri', 'dc.language.iso', 'dc.rights', 'dc.subject', 'dc.title'], boring=boring, alreadyharvested=alreadyharvested):
+            rec['autaff'][-1].append(publisher)
+            if fc:
+                rec['fc'] = fc
+            ejlmod3.printrecsummary(rec)
+            #print(rec['thesis.metadata.keys'])
+            recs.append(rec)
+        print('  %i records so far' % (len(recs)))
+        time.sleep(20)
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
-
