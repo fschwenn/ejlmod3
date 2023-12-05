6d0705ec0b78f73254ad7c9ce6b7eccd7db4e91f
Florian Schwennsen <florian.schwennsen@desy.de>
2023-12-05 09:32:04 +0100

    theses-maryland3: new DSpace-Version -> use  ejlmod3.ngrx()

--- active/theses-maryland3.py
+++ active/theses-maryland3.py
@@ -2,71 +2,66 @@
 #harvest Maryland University theses
 #FS: 2018-01-31
 #FS: 2022-11-18
+#FS: 2023-12-02
 
 import getopt
 import os
-import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 import re
 import ejlmod3
 import time
+import undetected_chromedriver as uc
 
 publisher = 'Maryland U., College Park'
 
 jnlfilename = 'THESES-MARYLAND-%s' % (ejlmod3.stampoftoday())
 
-rpp = 40
+rpp = 60
 pages = 1
 years = 2 
 skipalreadyharvested = True
+boring = ['Biophysics (BIPH)']
 
 
 if skipalreadyharvested:
     alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
+
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
+
+
 recs = []
-for  (fc, dep, aff) in [('m', '2793', 'Maryland U., College Park'), ('c', '2756', 'Maryland U., College Park'),
-                        ('a', '2746', 'Maryland U., College Park'), ('', '2800', 'Maryland U.')]:
+for (fc, dep) in [('m', '95a82c34-4a9e-4c60-b7ca-97e72aae87f8'),
+                  ('a', '99f30899-ac38-4c92-ba8e-93acf93ff8f5'),
+                  ('c', 'b7b14361-c1de-4b2c-8250-62ad1064f457'),
+                  ('', '5b1551c7-d9b5-4bed-aa6b-7bde543a32a1')]:
     for page in range(pages):
-        tocurl = 'https://drum.lib.umd.edu/handle/1903/' + dep + '/browse?rpp=' + str(rpp) + '&sort_by=2&type=dateissued&offset=' + str(page*rpp) + '&etal=-1&order=DESC'
-        ejlmod3.printprogress('=', [[dep, page+1, pages], [tocurl]])
+        tocurl = 'https://drum.lib.umd.edu/collections/' + dep + '?cp.page=' + str(page+1) + '&cp.rpp=' + str(rpp)
+        ejlmod3.printprogress('=', [[fc,dep], [page+1, pages], [tocurl]])
         try:
-            tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl), features="lxml")
-            time.sleep(4)
+            driver.get(tocurl)
+            time.sleep(5)
+            tocpage = BeautifulSoup(driver.page_source, features="lxml")
         except:
-            print("retry %s in 180 seconds" % (tocurl))
-            time.sleep(180)
-            tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl), features="lxml")
-        for rec in ejlmod3.getdspacerecs(tocpage, 'https://drum.lib.umd.edu'):
-            if 'year' in rec and int(rec['year']) <= ejlmod3.year(backwards=years):
-                print('     %s too old' % (rec['hdl']))
-            elif skipalreadyharvested and rec['hdl'] in alreadyharvested:
-                print('     %s already in backup' % (rec['hdl']))
-            else:
-                if fc: rec['fc'] = fc
-                rec['affiliation'] = aff
-                recs.append(rec)
-
-i = 0
-for rec in recs:
-    i += 1
-    ejlmod3.printprogress('-', [[i, len(recs)], [rec['link']]])
-    try:
-        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-        time.sleep(4)
-    except:
-        print("retry %s in 180 seconds" % (rec['link']))
-        time.sleep(180)
-        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-    ejlmod3.metatagcheck(rec, artpage, ['DCTERMS.abstract', 'DC.subject', 'DC.creator', 'bepress_citation_pdf_url',
-                                        'DC.identifier', 'citation_pdf_url'])
-    rec['autaff'][-1].append(rec['affiliation'])
-    for div in artpage.body.find_all('div', attrs = {'class' : 'simple-item-view-advisor'}):
-        for p in div.find_all('div'):
-            rec['supervisor'].append([ re.sub('^Dr. ', '', p.text.strip()) ])
-    for div in artpage.body.find_all('div', attrs = {'class' : 'simple-view-element'}):
-        for a in div.find_all('a'):
-            if a.has_attr('href') and re.search('10.13016\/', a['href']):
-                rec['doi'] = re.sub('.*(10.13016\/.*)', r'\1', a['href'])
-    ejlmod3.printrecsummary(rec)
-
+            time.sleep(60)
+            driver.get(tocurl)
+            time.sleep(5)
+            tocpage = BeautifulSoup(driver.page_source, features="lxml")
+        baseurl = 'https://drum.lib.umd.edu'
+        
+        for rec in ejlmod3.ngrx(tocpage, baseurl, ['dc.contributor.advisor', 'dc.contributor.author', 'dc.contributor.department', 'dc.date.issued', 'dc.description.abstract', 'dc.identifier.uri', 'dc.subject.pqcontrolled', 'dc.subject.pquncontrolled', 'dc.title'], boring=boring, alreadyharvested=alreadyharvested):
+            rec['autaff'][-1].append(publisher)
+            if fc:
+                rec['fc'] = fc
+            ejlmod3.printrecsummary(rec)
+            #print(rec['thesis.metadata.keys'])
+            recs.append(rec)
+        print('  %i records so far' % (len(recs)))
+        time.sleep(20)
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
+
+    
