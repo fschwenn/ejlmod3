16f0acbd744e1a3d98e802e0c00e0b78da8b0a6f
Florian Schwennsen <florian.schwennsen@desy.de>
2023-12-05 09:33:17 +0100

    theses-guelph3: new DSpace-Version -> use  ejlmod3.ngrx()

--- active/theses-guelph3.py
+++ active/theses-guelph3.py
@@ -2,6 +2,7 @@
 #harvest theses from Guelph U.
 #FS: 2022-04-20
 #FS: 2023-03-27
+#FS: 2023-11-30
 
 import sys
 import os
@@ -10,112 +11,86 @@ from bs4 import BeautifulSoup
 import re
 import ejlmod3
 import time
+import undetected_chromedriver as uc
 
 jnlfilename = 'THESES-GUELPH-%s' % (ejlmod3.stampoftoday())
 publisher = 'Guelph U.'
 
-rpp = 50
+rpp = 40
 pages = 10
 skipalreadyharvested = True
 years = 2
-boringdisciplines = ['Mechanical+Engineering', 'Environmental+Sciences', 'Animal+and+Poultry+Science',
-                     'Chemistry', 'Department+of+Animal+Biosciences', 'Department+of+Chemistry',
-                     'Department+of+Economics+and+Finance', 'Department+of+Family+Relations+and+Applied+Nutrition',
-                     'Department+of+Pathobiology', 'Department+of+Plant+Agriculture', 'Economics', 'Engineering',
-                     'Family+Relations+and+Applied+Nutrition', 'Pathobiology', 'Plant+Agriculture', 'Public+Health',
-                     'School+of+Engineering', 'Department+of+Population+Medicine', 'Bioinformatics', 'Geography', 
-                     'Biomedical+Sciences', 'Clinical+Studies', 'Creative+Writing', 'Department+of+Clinical+Studies', 
-                     'Criminology+and+Criminal+Justice+Policy', 'Department+of+Biomedical+Sciences', 'English',
-                     'Department+of+Geography%2C+Environment+and+Geomatics', 'Department+of+History', 'History', 
-                     'Department+of+Human+Health+and+Nutritional+Sciences', 'Department+of+Integrative+Biology',
-                     'Department+of+Marketing+and+Consumer+Studies', 'Department+of+Molecular+and+Cellular+Biology',
-                     'Department+of+Psychology', 'Department+of+Sociology+and+Anthropology', 'Doctor+of+Veterinary+Science',
-                     'Human+Health+and+Nutritional+Sciences', 'Integrative+Biology', 'Landscape+Architecture', 'Management', 
-                     'Latin+American+and+Caribbean+Studies', 'Literary+Studies+%2F+Theatre+Studies+in+English',
-                     'Molecular+and+Cellular+Biology', 'Psychology', 'Rural+Planning+and+Development', 'Sociology',
-                     'School+of+English+and+Theatre+Studies', 'School+of+Environmental+Design+and+Rural+Development',
-                     'School+of+Languages+and+Literatures', 'Theatre+Studies', 'Veterinary+Science', 'Philosophy',
-                     'Biophysics', 'Collaborative+International+Development+Studies', 'Department+of+Philosophy', 
-                     'Department+of+Food%2C+Agricultural+and+Resource+Economics', 'Department+of+Food+Science',
-                     'Department+of+Political+Science', 'Food%2C+Agriculture+and+Resource+Economics', 'Food+Science',
-                     'Political+Science', 'School+of+Environmental+Sciences', 'Tourism+and+Hospitality', 'Toxicology',
-                     'School+of+Hospitality%2C+Food+and+Tourism+Management', 'Applied+Statistics',
-                     'Department+of+Environmental+Biology', 'Department+of+Land+Resource+Science',
-                     'Environmental+Biology', 'Faculty+of+Environmental+Sciences', 'Land+Resource+Science',
-                     'Population+Medicine']
-boringdegrees = ['Master+of+Science', 'masters', 'Doctor+of+Education', 'Doctor+of+Musical+Arts',
-                 'Master+of+Music', 'Doctor+of+Business+Administration', 'Doctor+of+Occupational+Therapy',
-                 'Doctor+of+Ministry', 'Doctor+of+Public+Health', 'Doctor+of+Science+in+Dentistry',
-                 'Master+of+Applied+Science', 'Master+of+Arts', 'Master+of+Fine+Arts', 'Bachelor+of+Science',
-                 'Master+of+Business+Administration+%28Hospitality+and+Tourism+Management%29',
-                 'Master+of+Landscape+Architecture', 'Master+of+Science+%28Planning%29']
+boring = ['Mechanical Engineering', 'Environmental Sciences', 'Animal and Poultry Science',
+          'Chemistry', 'Department of Animal Biosciences', 'Department of Chemistry',
+          'Department of Economics and Finance', 'Department of Family Relations and Applied Nutrition',
+          'Department of Pathobiology', 'Department of Plant Agriculture', 'Economics', 'Engineering',
+          'Family Relations and Applied Nutrition', 'Pathobiology', 'Plant Agriculture', 'Public Health',
+          'School of Engineering', 'Department of Population Medicine', 'Bioinformatics', 'Geography', 
+          'Biomedical Sciences', 'Clinical Studies', 'Creative Writing', 'Department of Clinical Studies', 
+          'Criminology and Criminal Justice Policy', 'Department of Biomedical Sciences', 'English',
+          'Department of Geography%2C Environment and Geomatics', 'Department of History', 'History', 
+          'Department of Human Health and Nutritional Sciences', 'Department of Integrative Biology',
+          'Department of Marketing and Consumer Studies', 'Department of Molecular and Cellular Biology',
+          'Department of Psychology', 'Department of Sociology and Anthropology', 'Doctor of Veterinary Science',
+          'Human Health and Nutritional Sciences', 'Integrative Biology', 'Landscape Architecture', 'Management', 
+          'Latin American and Caribbean Studies', 'Literary Studies %2F Theatre Studies in English',
+          'Molecular and Cellular Biology', 'Psychology', 'Rural Planning and Development', 'Sociology',
+          'School of English and Theatre Studies', 'School of Environmental Design and Rural Development',
+          'School of Languages and Literatures', 'Theatre Studies', 'Veterinary Science', 'Philosophy',
+          'Biophysics', 'Collaborative International Development Studies', 'Department of Philosophy', 
+          'Department of Food%2C Agricultural and Resource Economics', 'Department of Food Science',
+          'Department of Political Science', 'Food%2C Agriculture and Resource Economics', 'Food Science',
+          'Political Science', 'School of Environmental Sciences', 'Tourism and Hospitality', 'Toxicology',
+          'School of Hospitality%2C Food and Tourism Management', 'Applied Statistics',
+          'Department of Environmental Biology', 'Department of Land Resource Science',
+          'Environmental Biology', 'Faculty of Environmental Sciences', 'Land Resource Science',
+          'Population Medicine', 'Master of Applied Science', 'Master of Fine Arts',
+          'Master of Landscape Architecture', 'Master of Science (Planning)', 'Master of Science',
+          'Animal Biosciences', 'Artificial Intelligence', 'Bioinformatics', 'Biomedical Sciences',
+          'Biophysics', 'Capacity Development and Extension', 'Chemistry', 'Clinical Studies',
+          'Creative Writing', 'Economics', 'Engineering', 'Environmental Sciences',
+          'Family Relations and Applied Nutrition', 'Food, Agriculture and Resource Economics',
+          'Food Science', 'Geography', 'Human Health and Nutritional Sciences', 'Integrative Biology',
+          'Landscape Architecture', 'Management', 'Marketing and Consumer Studies',
+          'Molecular and Cellular Biology', 'Neuroscience', 'Pathobiology', 'Philosophy',
+          'Plant Agriculture', 'Political Science', 'Population Medicine', 'Psychology',
+          'Regenerative Medicine', 'Rural Planning and Development', 'Rural Studies', 'Sociology',
+          'Tourism and Hospitality', 'Master of Science (Aquaculture)', 'Applied Nutrition',
+          'Literary Studies / Theatre Studies in English']
 
 if skipalreadyharvested:
     alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
 else:
     alreadyharvested = []
 
-hdr = {'User-Agent' : 'Magic Browser'}
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
+
+baseurl = 'https://atrium.lib.uoguelph.ca'
+driver.get(baseurl)
+time.sleep(10)    
+
 
 recs = []
-redeg = re.compile('rft.degree=')
 for page in range(pages):
-    tocurl = 'https://atrium.lib.uoguelph.ca/xmlui/handle/10214/151/browse?order=DESC&rpp='+str(rpp)+'&sort_by=3&etal=-1&offset=' + str((page)*rpp) + '&type=dateissued'
-    ejlmod3.printprogress("=", [[page+1, pages], [tocurl]])
-    req = urllib.request.Request(tocurl, headers=hdr)
-    tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-    for rec in ejlmod3.getdspacerecs(tocpage, 'https://atrium.lib.uoguelph.ca', alreadyharvested=alreadyharvested):
-        if 'year' in rec and int(rec['year']) <= ejlmod3.year(backwards=years):
-            keepit = False            
-            #print( '  skip',  rec['year'])
-        else:
-            keepit = True
-            for degree in rec['degrees']:
-                if degree in boringdisciplines or degree in boringdegrees:
-                    keepit = False
-                    #print( '  skip', degree)
-        if keepit:
-            recs.append(rec)
-    print('  %i records so far' % (len(recs)))
-    time.sleep(3)
-
-i = 0
-for rec in recs:
-    i += 1
-    ejlmod3.printprogress("-", [[i, len(recs)], [rec['link']]])
+    tocurl = 'https://atrium.lib.uoguelph.ca/collections/50bed0cb-5490-4258-aec6-a4ec4a410bef?cp.page=' + str(page+1) + '&cp.rpp=' + str(rpp)
+    ejlmod3.printprogress('=', [[page+1, pages], [tocurl]])
     try:
-        req = urllib.request.Request(rec['link'] + '?show=full', headers=hdr)
-        artpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-        time.sleep(4)
+        driver.get(tocurl)
+        time.sleep(5)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
     except:
-        try:
-            print("retry %s in 180 seconds" % (rec['link']))
-            time.sleep(180)
-            req = urllib.request.Request(rec['link'] + '?show=full', headers=hdr)
-            artpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-        except:
-            print("no access to %s" % (rec['link']))
-            continue    
-    ejlmod3.metatagcheck(rec, artpage, ['DC.creator', 'citation_title', 'citation_date',
-                                        'DCTERMS.abstract', 'citation_pdf_url',
-                                        'citation_keywords', 'DC.rights'])
-    for tr in artpage.body.find_all('tr'):
-        tdt = ''
-        for td in tr.find_all('td', attrs = {'class' : 'label-cell'}):
-            tdt = td.text.strip()
-            td.decompose()
-        for td in tr.find_all('td'):
-            if td.text.strip() == 'en_US':
-                continue
-            #supervisor
-            if tdt in ['dc.contributor.supervisor', 'dc.contributor.advisor']:
-                if td.text.strip():
-                    rec['supervisor'].append([ re.sub(' \(.*', '', td.text.strip()) ])
-            #ORCID
-            elif tdt == 'dc.identifier.orcid':
-                if re.search('\d\d\d\d\-\d\d\d\d', td.text):
-                    rec['autaff'][-1].append('ORCID:' + re.sub('.*orcid.org\/+', '', td.text.strip()))
-    rec['autaff'][-1].append(publisher)
-    ejlmod3.printrecsummary(rec)
-
+        time.sleep(60)
+        driver.get(tocurl)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    
+    for rec in ejlmod3.ngrx(tocpage, baseurl, ['dc.contributor.advisor', 'dc.contributor.author', 'dc.degree.name', 'dc.date.created', 'dc.degree.programme', 'dc.description.abstract', 'dc.identifier.uri',  'dc.rights.license', 'dc.subject', 'dc.title'], boring=boring, alreadyharvested=alreadyharvested):
+        rec['autaff'][-1].append(publisher)
+        ejlmod3.printrecsummary(rec)
+        #print(rec['thesis.metadata.keys'])
+        recs.append(rec)
+    print('  %i records so far' % (len(recs)))
+    time.sleep(20)
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
