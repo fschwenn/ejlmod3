3228ff0f952e91852ddd02afbf1f40a4a8850e8a
Florian Schwennsen <florian.schwennsen@desy.de>
2023-12-05 09:32:10 +0100

    theses-calgary3: new DSpace-Version -> use  ejlmod3.ngrx()

--- active/theses-calgary3.py
+++ active/theses-calgary3.py
@@ -1,80 +1,78 @@
 # -*- coding: utf-8 -*-
 #harvest theses from Calgary
 #FS: 2020-11-21
+#FS: 2023-11-28
 
-import urllib.request, urllib.error, urllib.parse
-import urllib.parse
 from bs4 import BeautifulSoup
 import re
 import ejlmod3
 import time
+import undetected_chromedriver as uc
+import os
+
 
 publisher = 'Calgary U.'
-rpp = 50
-numofpages = 1
+rpp = 40
+pages = 10
+skipalreadyharvested = True
+
 jnlfilename = 'THESES-CALGARY-%s' % (ejlmod3.stampoftoday())
+if skipalreadyharvested:
+    alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
 
 boring = ['Biological Sciences', 'Chemistry', 'Geoscience', 'Psychology – Clinical', 'Economics', 'Medicine – Neuroscience']
 boring += ['Biological+Sciences', 'Chemistry', 'Geoscience', 'Psychology+%E2%80%93+Clinical', 'Medicine+%E2%80%93+Neuroscience']
+boring += ['Archaeology', 'Communication and Media Studies', 'Medicine – Immunology',
+           'Education Graduate Program – Educational Psychology',
+           'Education Graduate Program – Educational Research', 'Engineering – Biomedical',
+           'Engineering – Chemical &a; Petroleum', 'Engineering – Civil', 'Engineering – Geomatics',
+           'Engineering – Mechanical &a; Manufacturing', 'English', 'Kinesiology',
+           'Languages, Literatures and Cultures', 'Law', 'Linguistics',
+           'Medicine – Biochemistry and Molecular Biology', 'Medicine – Community Health Sciences',
+           'Medicine – Gastrointestinal Sciences', 'Medicine – Medical Sciences',
+           'Medicine – Microbiology &a; Infectious Diseases', 'Military &a; Strategic Studies',
+           'Music', 'Nursing', 'Political Science', 'Psychology', 'Social Work', 'Sociology',
+           'Veterinary Medical Sciences', 'Urban and Regional Planning', 'Geography',
+           'Business, Haskayne School of Business', 'Computational Media Design',
+           'Environmental Design', 'Haskayne School of Business: Management', 'History',
+           'Religious Studies', 'Accounting', 'Bioinformatics', 'Business Administration--Management',
+           'Economics--Agricultural', 'Education--Business', 'Education--Health', 'Education--Religious',
+           'Education--Technology', 'History--African', 'History--European', 'History--Modern', 'Theater',
+           'Urban and Regional Planning', 'Anthropology', 'French, Italian and Spanish',
+           'Medicine – Cardiovascular/Respiratory Science', 'Biology--Bioinformatics', 'Biology--Cell',
+           'Biology--Molecular', 'Biology', 'Biophysics--Medical', 'Economics--Finance',
+           'Education--Bilingual and Multicultural', 'Education--Sciences', 'Education',
+           'Engineering--Biomedical', 'Environmental Sciences', 'Geography', 'Geophysics', 'Immunology',
+           'Language', 'Oncology', 'Psychology--Industrial', 'Virology', 'Biochemistry',
+           'Biological Sciences', 'Biology', 'Chemistry', 'Disaster Risk and Resilience', 'Ecology',
+           'Environmental Chemistry', 'Environmental Sciences', 'Psychology',
+           'Speech and Language Sciences', 'Water Resource Management']
 
-hdr = {'User-Agent' : 'Magic Browser'}
-prerecs = []
-for i in range(numofpages):
-    tocurl = 'https://prism.ucalgary.ca/handle/1880/100031/discover?rpp=' + str(rpp) + '&etal=0&scope=&group_by=none&page=' + str(i+1) + '&sort_by=dc.date.issued_dt&order=desc&filtertype_0=pubFaculty&filtertype_1=degree&filter_relational_operator_1=equals&filter_relational_operator_0=equals&filter_1=Doctor+of+Philosophy+%28PhD%29&filter_0=Science'
-    ejlmod3.printprogress('=', [[i+1, numofpages], [tocurl]])
-    req = urllib.request.Request(tocurl, headers=hdr)
-    tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-    for rec in ejlmod3.getdspacerecs(tocpage, 'https://prism.ucalgary.ca'):
-        if ejlmod3.checkinterestingDOI(rec['hdl']):
-            keepit = True            
-            if 'degrees' in rec:
-                for deg in rec['degrees']:
-                    if deg in boring:
-                        keepit = False
-            if keepit: 
-                prerecs.append(rec)
-    print('  %4i records so far' % (len(prerecs)))
-    time.sleep(3)
-            
-j = 0
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
+
 recs = []
-for rec in prerecs:
-    keepit = True
-    j += 1
-    ejlmod3.printprogress('-', [[j, len(prerecs)], [rec['link']], [len(recs)]])
-    req = urllib.request.Request(rec['link']+'?show=full')
-    artpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-    time.sleep(5)
-    ejlmod3.metatagcheck(rec, artpage, ['citation_date', 'DC.creator', 'DC.subject',
-                                        'DC.rights', 'citation_pdf_url', 'DC.identifier',
-                                        'DCTERMS.abstract'])
-    rec['autaff'][-1].append(publisher)                        
-    #supervisor
-    for tr in artpage.body.find_all('tr', attrs = {'class' : 'ds-table-row'}):
-        (label, word) = ('', '')
-        for td in tr.find_all('td', attrs = {'class' : 'label-cell'}):
-            label = td.text.strip()
-        for td in tr.find_all('td', attrs = {'class' : 'word-break'}):
-            word = td.text.strip()
-        #department
-        if re.search('thesis.degree.discipline', label):
-            if word in boring:
-                keepit = False
-            elif word == 'Mathematics & Statistics':
-                rec['fc'] = 'm'
-            elif word == 'Computer Science':
-                rec['fc'] = 'c'
-            elif word != 'Physics & Astronomy':
-                rec['note'].append(word)
-        #supervisor
-        elif re.search('dc.contributor.advisor', label):
-            rec['supervisor'].append(word)
-    if keepit:
+for page in range(pages):
+    tocurl = 'https://prism.ucalgary.ca/collections/16fe099b-62b7-45bd-9a99-5fc465f0d04d?cp.page=' + str(page+1) + '&cp.rpp=' + str(rpp)
+    ejlmod3.printprogress('=', [[page+1, pages], [tocurl]])
+    try:
+        driver.get(tocurl)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    except:
+        time.sleep(60)
+        driver.get(tocurl)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    baseurl = 'https://prism.ucalgary.ca'
+    
+    for rec in ejlmod3.ngrx(tocpage, baseurl, ['dc.contributor.advisor', 'dc.contributor.author', 'dc.date.issued', 'dc.title', 'dc.identifier.uri', 'dc.description.abstract', 'dc.identifier.uri', 'dc.language.iso', 'dc.rights', 'dc.subject', 'dc.subject.classification', 'thesis.degree.discipline', 'thesis.degree.name'], boring=boring, alreadyharvested=alreadyharvested):
+        rec['autaff'][-1].append(publisher)
         ejlmod3.printrecsummary(rec)
+        #print(rec['thesis.metadata.keys'])
         recs.append(rec)
-    else:
-        ejlmod3.adduninterestingDOI(rec['hdl'])
-        
-
+    print('  %i records so far' % (len(recs)))
+    time.sleep(20)
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
-
