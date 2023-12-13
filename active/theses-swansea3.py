94280c585f72cbb0ee08d6dc46eae1913018b776
Florian Schwennsen <florian.schwennsen@desy.de>
2023-12-13 09:54:16 +0100

    theses-swansea3: major rewrite

--- active/theses-swansea3.py
+++ active/theses-swansea3.py
@@ -17,45 +17,62 @@ recs = []
 
 publisher = 'Swansea U.'
 jnlfilename = 'THESES-SWANSEA-%s' % (ejlmod3.stampoftoday())
-pages = 2
+pages = 2+5
 skipalreadyharvested = True
 
 if skipalreadyharvested:
     alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
 
 def http_request(url):
-    hdr = {'User-Agent' : 'Magic Browser'}
-    req = urllib.request.Request(url, headers=hdr)
-    soup = BeautifulSoup(urllib.request.urlopen(req).read(), 'lxml')
+    try:
+        soup = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(url), features="lxml")
+        req = urllib.request.Request(url, headers=hdr)
+        soup = BeautifulSoup(urllib.request.urlopen(req).read(), 'lxml')
+    except:
+        sleep(60)
+        req = urllib.request.Request(url, headers=hdr)
+        soup = BeautifulSoup(urllib.request.urlopen(req).read(), 'lxml')
     return soup
 
 
 def get_subside(url):
-    soap = http_request(url)
     print("["+ url + "] --> Harvesting data")
     rec = {'tc' : 'T', 'jnl' : 'BOOK'}
+    try:
+        soup = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(url), features="lxml")
+        ejlmod3.metatagcheck(rec, soup, ['citation_author', 'citation_doi'])
+        rec['autaff']
+    except:
+        sleep(60)
+        soup = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(url), features="lxml")
+        ejlmod3.metatagcheck(rec, soup, ['citation_author', 'citation_doi'])
+        
 
     # Get the title
-    title_section = soap.find_all('strong', attrs={'property': 'name'})
+    title_section = soup.find_all('strong', attrs={'property': 'name'})
     if len(title_section) == 1:
         rec['tit'] = title_section[0].text
 
     # Get the author's name
-    author_section = soap.find_all('span', attrs={'property': 'author'})
+    author_section = soup.find_all('span', attrs={'property': 'author'})
     if len(author_section) == 1:
         rec['autaff'] = [[author_section[0].text ]]
         if not 'autaff' in list(rec.keys()):
-            for meta in soap.find_all('meta', attrs={'name' : 'citation_author'}):
+            for meta in soup.find_all('meta', attrs={'name' : 'citation_author'}):
                 rec['autaff'] = [[ meta['content'] ]]
-    rec['autaff'][-1].append(publisher)
+    if not 'autaff' in list(rec.keys()):
+        print('   no author found')
+        return
+    else:
+        rec['autaff'][-1].append(publisher)
 
     # Get DOI
-    doi_section = soap.find_all('a', attrs={'class': 'online'})
+    doi_section = soup.find_all('a', attrs={'class': 'online'})
     if len(doi_section) == 1:
         rec['doi'] = doi_section[0].text
 
     # Get the abstract
-    table_section = soap.find_all('div', attrs={'class': 'description-tab'})
+    table_section = soup.find_all('div', attrs={'class': 'description-tab'})
     if len(table_section) == 1:
         table_rows = table_section[0].find_all('tr')
         for tr in table_rows:
@@ -66,7 +83,7 @@ def get_subside(url):
                     if len(abstract_raw) == 1:
                         rec['abs'] = abstract_raw[0].text.replace('\n', '').replace('                  ', '')
 
-    details_section = soap.find_all('table', attrs={'summary': 'Bibliographic Details'})
+    details_section = soup.find_all('table', attrs={'summary': 'Bibliographic Details'})
     if len(details_section) == 1:
         for tr in details_section[0].find_all('tr'):
             th = tr.find_all('th')
@@ -89,13 +106,13 @@ def get_subside(url):
                     #print rec['supervisor']
 
     # Get the pdf link
-    pdf_section = soap.find_all('a', attrs={'class': 'file-download'})
+    pdf_section = soup.find_all('a', attrs={'class': 'file-download'})
     if len(pdf_section) == 1:
         pdf_link = pdf_section[0].get('href')
         rec['hidden'] = "https://cronfa.swan.ac.uk" + pdf_link
     
     # Get the Keywords
-    description_table = soap.find_all('table', attrs={'summary': 'Description'})
+    description_table = soup.find_all('table', attrs={'summary': 'Description'})
     if len(description_table) == 1:
         rows = description_table[0].find_all('tr')
         for tr in rows:
@@ -116,10 +133,12 @@ def get_subside(url):
 
 for page in range(1, pages+1):
     url = "https://cronfa.swan.ac.uk/Search/Results?type=AllFields&sort=year&filter%5B%5D=college_str%3A%22College+of+Science%22&filter%5B%5D=format%3A%22E-Thesis%22&page=" + str(page)
-    print("--- Open page " + str(page) + " --- " + url)
-
-    for link in http_request(url).find_all('a', attrs={'class': 'title'}):
+    url = 'https://cronfa.swan.ac.uk/Search/Results?join=AND&bool0%5B%5D=AND&lookfor0%5B%5D=&type0%5B%5D=AllFields&filter%5B%5D=%7Eformat%3A%22E-Thesis%22&filter%5B%5D=college_str%3A%22Faculty+of+Science+and+Engineering%22&page=' + str(page)
+    ejlmod3.printprogress('=', [[page, pages], [url]])
+    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(url), features="lxml")
+    sleep(5)
+    for link in tocpage.find_all('a', attrs={'class': 'title'}):
         get_subside("https://cronfa.swan.ac.uk" + link.get('href'))
-        sleep(10)
+        sleep(5)
 
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
