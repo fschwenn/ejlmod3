c1672761c705cf64a7fb317af3e30feff16285f0
Florian Schwennsen <florian.schwennsen@desy.de>
2024-01-16 11:13:04 +0100

    ejlmod3: KB updated

--- lib/ejlmod3.py
+++ lib/ejlmod3.py
@@ -96,7 +96,7 @@ reorcid = re.compile('^ORCID:\d{4}\-\d{4}\-\d{4}\-\d{3}[0-9X]$')
 #(can also handle mutiple FCs like 'ai' or so)
 #from inspirelibrarylabs import fcjournalliste
 fcjournalliste = [('b', ['IEEE Trans.Appl.Supercond.', 'Supercond.Sci.Technol.', 'JACoW']),
-                  ('m', ['Abstr.Appl.Anal.', 'Acta Appl.Math.', 'Adv.Appl.Clifford Algebras', 'Adv.Math.', 'Adv.Math.Phys.', 'Afr.Math.', 'Alg.Anal.', 'Algebr.Geom.Topol.', 'Alg.Groups Geom.', 'Alg.Logika', 'Anal.Math.Phys.', 'Anal.Part.Diff.Eq.', 'Annals Probab.', 'Ann.Inst.H.Poincare Probab.Statist.', 'Ann.Math.Sci.Appl.', 'Ann.PDE', 'Arab.J.Math.', 'Asian J.Math.', 'Axioms', 'Bayesian Anal.', 'Braz.J.Probab.Statist.', 'Bull.Am.Math.Soc.', 'Bull.Austral.Math.Soc.', 'Cahiers Topo.Geom.Diff.', 'Calc.Var.Part.Differ.Equ', 'Can.J.Math.', 'Commun.Anal.Geom.', 'Commun.Math.Phys.', 'Commun.Math.Sci.', 'Commun.Pure Appl.Math.', 'Compos.Math.', 'Compt.Rend.Math.', 'Conform.Geom.Dyn.', 'Contemp.Math.', 'Duke Math.J.', 'Eur.J.Combinatorics', 'Exper.Math.', 'Forum Math.Pi', 'Forum Math.Sigma', 'Fractals', 'Geom.Topol.', 'Geom.Topol.Monographs', 'Glasgow Math.J.', 'Hokkaido Math.J.', 'Int.Math.Res.Not.', 'Invent.Math.', 'Inverse Prob.', 'Izv.Vuz.Mat.', 'J.Alg.Geom.', 'J.Am.Math.Soc.', 'J.Appl.Math.', 'J.Appl.Math.Mech.', 'J.Austral.Math.Soc.', 'J.Diff.Geom.', 'J.Geom.Anal.', 'J.Geom.Symmetry Phys.', 'J.Inst.Math.Jussieu', 'J.Integrab.Syst.', 'J.Korean Math.Soc.', 'J.Math.Phys.', 'J.Math.Res.', 'J.Math.Sci.', 'J.Math.Soc.Jap.', 'J.Part.Diff.Eq.', 'Lect.Notes Math.', 'Lett.Math.Phys.', 'Manuscr.Math.', 'Math.Comput.', 'Mathematics', 'Math.Methods Appl.Sci.', 'Math.Nachr.', 'Math.Notes', 'Math.Phys.Anal.Geom.', 'Math.Phys.Stud.', 'Math.Proc.Cambridge Phil.Soc.', 'Math.Res.Lett.', 'Mat.Sbornik', 'Mat.Zametki', 'Moscow Math.J.', 'Pacific J.Math.', 'p Adic Ultra.Anal.Appl.', 'Proc.Am.Math.Soc.', 'Proc.Am.Math.Soc.Ser.B', 'Proc.Geom.Int.Quant.', 'Prog.Math.Phys.', 'Rept.Math.Phys.', 'Russ.J.Math.Phys.', 'Russ.Math.Surveys', 'Springer Proc.Math.Stat.', 'Tokyo J.Math.', 'Trans.Am.Math.Soc.', 'Trans.Am.Math.Soc.Ser.B', 'Trans.Moscow Math.Soc.', 'Turk.J.Math.', 'Ukr.Math.J.', 'J.Reine Angew.Math.', 'Arch.Ration.Mech.Anal.', 'Acta Math.Vietnamica', 'Quart.J.Math.Oxford Ser.', 'Int.J.Math.', 'Integral Transform.Spec.Funct.', 'Commun.Contemp.Math.', 'Selecta Math.', 'J.Sympl.Geom.', 'Q.Appl.Math.', 'J.Universal Math.', 'Anal.Geom.Metr.Spaces', 'Rev.Roum.Math.Pures Appl.', 'GESJ Math.Mech.', 'Comp.Meth.Appl.Math.', 'Transform.Groups', 'Rev.Mate.Teor.Aplic.', 'Combin.Theor.', 'Forum Math.', 'Compl.Manif.', 'Commun.Math.', 'SIAM J.Math.Anal.', 'SIAM J.Appl.Math.', 'SIAM J.Appl.Math.', 'SIAM J.Matrix Anal.Appl.', 'SIAM J.Numer.Anal.', 'SIAM J.Discrete Math.', 'SIAM J.Appl.Alg.Geom.', 'Filomat', 'Math.Slovaca', 'Math.Ann.', 'Am.J.Math.', 'Algorithmica', 'Funct.Anal.Appl.', 'Numer.Math.', 'Probab.Theor.Related Fields', 'Combinatorica', 'Isr.J.Math.', 'Adv.Comput.Math.', 'Annals Global Anal.Geom.', 'Geom.Funct.Anal.', 'Annali Mat.Pura Appl.', 'Funkt.Anal.Pril.',  'Proc.Indian Acad.Sci.A', 'Jap.J.Math.', 'Algorithmica', 'Proc.Lond.Math.Soc.']),
+                  ('m', ['Abstr.Appl.Anal.', 'Acta Appl.Math.', 'Adv.Appl.Clifford Algebras', 'Adv.Math.', 'Adv.Math.Phys.', 'Afr.Math.', 'Alg.Anal.', 'Algebr.Geom.Topol.', 'Alg.Groups Geom.', 'Alg.Logika', 'Anal.Math.Phys.', 'Anal.Part.Diff.Eq.', 'Annals Probab.', 'Ann.Inst.H.Poincare Probab.Statist.', 'Ann.Math.Sci.Appl.', 'Ann.PDE', 'Arab.J.Math.', 'Asian J.Math.', 'Axioms', 'Bayesian Anal.', 'Braz.J.Probab.Statist.', 'Bull.Am.Math.Soc.', 'Bull.Austral.Math.Soc.', 'Cahiers Topo.Geom.Diff.', 'Calc.Var.Part.Differ.Equ', 'Can.J.Math.', 'Commun.Anal.Geom.', 'Commun.Math.Phys.', 'Commun.Math.Sci.', 'Commun.Pure Appl.Math.', 'Compos.Math.', 'Compt.Rend.Math.', 'Conform.Geom.Dyn.', 'Contemp.Math.', 'Duke Math.J.', 'Eur.J.Combinatorics', 'Exper.Math.', 'Forum Math.Pi', 'Forum Math.Sigma', 'Fractals', 'Geom.Topol.', 'Geom.Topol.Monographs', 'Glasgow Math.J.', 'Hokkaido Math.J.', 'Int.Math.Res.Not.', 'Invent.Math.', 'Inverse Prob.', 'Izv.Vuz.Mat.', 'J.Alg.Geom.', 'J.Am.Math.Soc.', 'J.Appl.Math.', 'J.Appl.Math.Mech.', 'J.Austral.Math.Soc.', 'J.Diff.Geom.', 'J.Geom.Anal.', 'J.Geom.Symmetry Phys.', 'J.Inst.Math.Jussieu', 'J.Integrab.Syst.', 'J.Korean Math.Soc.', 'J.Math.Phys.', 'J.Math.Res.', 'J.Math.Sci.', 'J.Math.Soc.Jap.', 'J.Part.Diff.Eq.', 'Lect.Notes Math.', 'Lett.Math.Phys.', 'Manuscr.Math.', 'Math.Comput.', 'Mathematics', 'Math.Methods Appl.Sci.', 'Math.Nachr.', 'Math.Notes', 'Math.Phys.Anal.Geom.', 'Math.Phys.Stud.', 'Math.Proc.Cambridge Phil.Soc.', 'Math.Res.Lett.', 'Mat.Sbornik', 'Mat.Zametki', 'Moscow Math.J.', 'Pacific J.Math.', 'p Adic Ultra.Anal.Appl.', 'Proc.Am.Math.Soc.', 'Proc.Am.Math.Soc.Ser.B', 'Proc.Geom.Int.Quant.', 'Prog.Math.Phys.', 'Rept.Math.Phys.', 'Russ.J.Math.Phys.', 'Russ.Math.Surveys', 'Springer Proc.Math.Stat.', 'Tokyo J.Math.', 'Trans.Am.Math.Soc.', 'Trans.Am.Math.Soc.Ser.B', 'Trans.Moscow Math.Soc.', 'Turk.J.Math.', 'Ukr.Math.J.', 'J.Reine Angew.Math.', 'Arch.Ration.Mech.Anal.', 'Acta Math.Vietnamica', 'Quart.J.Math.Oxford Ser.', 'Int.J.Math.', 'Integral Transform.Spec.Funct.', 'Commun.Contemp.Math.', 'Selecta Math.', 'J.Sympl.Geom.', 'Q.Appl.Math.', 'J.Universal Math.', 'Anal.Geom.Metr.Spaces', 'Rev.Roum.Math.Pures Appl.', 'GESJ Math.Mech.', 'Comp.Meth.Appl.Math.', 'Transform.Groups', 'Rev.Mate.Teor.Aplic.', 'Combin.Theor.', 'Forum Math.', 'Compl.Manif.', 'Commun.Math.', 'SIAM J.Math.Anal.', 'SIAM J.Appl.Math.', 'SIAM J.Appl.Math.', 'SIAM J.Matrix Anal.Appl.', 'SIAM J.Numer.Anal.', 'SIAM J.Discrete Math.', 'SIAM J.Appl.Alg.Geom.', 'Filomat', 'Math.Slovaca', 'Math.Ann.', 'Am.J.Math.', 'Algorithmica', 'Funct.Anal.Appl.', 'Numer.Math.', 'Probab.Theor.Related Fields', 'Combinatorica', 'Isr.J.Math.', 'Adv.Comput.Math.', 'Annals Global Anal.Geom.', 'Geom.Funct.Anal.', 'Annali Mat.Pura Appl.', 'Funkt.Anal.Pril.',  'Proc.Indian Acad.Sci.A', 'Jap.J.Math.', 'Algorithmica', 'Proc.Lond.Math.Soc.', 'Sbornik Math.']),
                   ('q', ['ACS Photonics', 'Atoms', 'J.Chem.Phys.', 'J.Chem.Theor.Comput.', 'J.Mod.Opt.', 'J.Molec.Struc.', 'J.Opt.', 'J.Opt.Soc.Am. A', 'J.Opt.Soc.Am. B', 'Mater.Chem.Phys.', 'Nano Lett.', 'Nanotechnol.', 'Nature Photon.']),
                   ('k', ['ACM Trans.Quant.Comput.', 'Quant.Inf.Proc.', 'Quantum Eng.', 'Quantum Rep.', 'Quantum Sci.Technol.', 'Quantum', 'AVS Quantum Sci.', 'Adv.Quantum Technol.', 'Mat.Quant.Tech.', 'APL Quantum', 'Quant.Inf.Comput.']),
                   ('f', ['Adv.Cond.Mat.Phys.', 'Ann.Rev.Condensed Matter Phys.', 'Condens.Mat.', 'J.Noncryst.Solids', 'J.Phys.Chem.Solids', 'J.Phys.Condens.Matter', 'Solid State Commun.', 'Sov.Phys.Solid State', 'Condensed Matter Phys.', 'Phys.Status Solidi', 'Solid State Phenom.', ' Phys.Status Solidi B', 'Phys.Status Solidi A']),
@@ -1966,6 +1966,7 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
 #            printprogress('-', [[i+1, len(links)], [link], [j]])
             if 'data' in metadata[link]:
                 keepit = True
+                fakedoi = False
                 thesis = metadata[link]['data']
                 if 'isWithdrawn' in thesis and thesis['isWithdrawn']:
                     continue
@@ -1973,7 +1974,7 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
                     rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK', 'supervisor' : [], 'note' : [],
                            'autaff' : [], 'degree' : [], 'fac' : []}
                     if fakehdl:
-                        rec['doi'] = '30.3000/fakehdl/' +  thesis['handle']
+                        fakedoi = '30.3000/fakehdl/' +  thesis['handle']
                     else:
                         rec['hdl'] = thesis['handle']
                     rec['thesis.metadata.keys'] = thesis['metadata'].keys()
@@ -2019,13 +2020,17 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
                         for pid in thesis['metadata'][key]:
                             rec['doi'] =  re.sub('^\/', '', pid['value'])
                         done.append(key)
+                    elif key in ['dc.identifier.urn']:
+                        for pid in thesis['metadata'][key]:
+                            rec['urn'] = pid['value']
+                        done.append(key)
                     elif key in ['dc.identifier.uri']:
                         for pid in thesis['metadata'][key]:
                             if re.search('doi.org\/10', pid['value']):
                                 rec['doi'] = re.sub('.*doi.org\/', '', pid['value'])
                             elif re.search('hdl.handle.net\/', pid['value']):
                                 if fakehdl:
-                                    rec['doi'] = '30.3000/fakehdl/' +  re.sub('.*hdl.handle.net\/', '', pid['value'])
+                                    fakedoi = '30.3000/fakehdl/' +  re.sub('.*hdl.handle.net\/', '', pid['value'])
                                 else:
                                     rec['hdl'] = re.sub('.*hdl.handle.net\/', '', pid['value'])
                             else:
@@ -2039,7 +2044,7 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
                                  'local.contributor.corporatename', 'dc.degree.programme',
                                  'unsw.relation.faculty', 'unsw.relation.school',
                                  'dc.contributor.other', 'dc.department',
-                                 'dc.thesis.degreediscipline']:
+                                 'dc.thesis.degreediscipline', 'local.subject.fakultaet']:
                         for fac in thesis['metadata'][key]:
                             if fac['value'] in boring:
                                 print('    skip "%s"' % (fac['value']))
@@ -2048,7 +2053,9 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
                                                   'Department of Mathematics and Statistics',
                                                   'School of Mathematics &a; Statistics',
                                                   'Departament d&s;Anàlisi Matemàtica',
-                                                  'Departament de Matemàtiques']:
+                                                  'Departament de Matemàtiques',
+                                                  'Naturwissenschaftliche Fakultät / Department Mathematik',
+                                                  'Friedrich-Alexander-Universität Erlangen-Nürnberg (FAU), Naturwissenschaftliche Fakultät, Department Mathematik, Lehrstuhl für Algebra und Geometrie (Knop)']:
                                 rec['fc'] = 'm'
                             elif fac['value'] in ['Statistics', 'Mathematical Statistics',
                                                   'Statistical Science']:
@@ -2056,15 +2063,21 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
                             elif fac['value'] in ['Centre for Quantum Computation &a; Communication Technology']:
                                 rec['fc'] = 'k'
                             elif fac['value'] in ['Astronomy', 'Astronomy and Astrophysics',
-                                                  'Departament d&s;Astronomia i Astrofísica']:
+                                                  'Departament d&s;Astronomia i Astrofísica',
+                                                  'Physikalisches Institut / Lehrstuhl für Experimentalphysik (Astroteilchenphysik)',
+                                                  'Erlangen Centre for Astroparticle Physics (ECAP)',
+                                                  'Physikalisches Institut / Lehrstuhl für Experimentalphysik (Astroteilchenphysik)']:
                                 rec['fc'] = 'a'
+                            elif fac['value'] in ['Institute for Theoretical Physics III, Chair for Quantum Gravity']:
+                                rec['fc'] = 'g'
                             elif fac['value'] in ['Computer Science', 'Computational Science and Engineering',
                                                   'Department of Computer Science',
                                                   'Departament d&s;Informàtica',
                                                   'Computer Science and#38; Applications',
                                                   'Computer Science and Applications',
                                                   'Computer Science &a; Applications',
-                                                  'Computer Engineering', 'Computer Science']:
+                                                  'Computer Engineering', 'Computer Science',
+                                                  'Technische Fakultät / Department Informatik']:
                                 rec['fc'] = 'c'
                             elif fac['value'] in ['Condensed Matter']:
                                 rec['fc'] = 'f'
@@ -2075,7 +2088,11 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
                                                       'Departament de Física Atòmica, Molecular i Nuclear',
                                                       'Departament de Física de la Terra i Termodinàmica',
                                                       'Departament de Fisica Teòrica',
-                                                      'CIENCIAS FÍSICO MATEMÁTICAS Y CIENCIAS DE LA TIERRA']:
+                                                      'CIENCIAS FÍSICO MATEMÁTICAS Y CIENCIAS DE LA TIERRA',
+                                                      'Naturwissenschaftliche Fakultät / Department Physik',
+                                                      'Department Physik', 'Naturwissenschaftliche Fakultät',
+                                                      'Department Physik / Professur für Theoretische Physik (Prof. Dr. Smith)',
+                                                      'Naturwissenschaftliche Fakultät / Naturwissenschaftliche Fakultät -ohne weitere Spezifikation-']:
                                 rec['fac'].append(fac['value'])
                                 rec['note'].append('%s=%s' % (key, fac['value']))
                         done.append(key)
@@ -2109,6 +2126,10 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
                         for extent in thesis['metadata'][key]:
                             if re.search('\d\d p', extent['value']):
                                 rec['pages'] = re.sub('.*?(\d\d+) p.*', r'\1', extent['value'])
+                            elif re.search('^\d+$', extent['value']):
+                                rec['pages'] = extent['value']
+                            elif re.search('\d\d Seiten', extent['value']):
+                                rec['pages'] = re.sub('.*?(\d\d+) Seiten.*', r'\1', extent['value'])
                             elif re.search('\d\d leaves', extent['value']):
                                 rec['pages'] = re.sub('.*?(\d\d+) leaves.*', r'\1', extent['value'])
                             else:
@@ -2187,7 +2208,11 @@ def ngrx(tocpage, urltrunc, listofkeys, fakehdl=False, boring=[], alreadyharvest
                         print('    %s already in backup' % (rec['doi']))
                     elif 'hdl' in rec and rec['hdl'] in alreadyharvested:
                         print('    %s already in backup' % (rec['hdl']))
+                    elif 'urn' in rec and rec['urn'] in alreadyharvested:
+                        print('    %s already in backup' % (rec['urn']))
                     else:
+                        if not 'urn' in rec and not 'doi' in rec and fakedoi:
+                            rec['doi'] = fakedoi
                         prerecs.append(rec)
     print('  [ngrx] %i/%i' % (len(prerecs), j))
     return prerecs
