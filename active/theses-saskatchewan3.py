26f9aba04f2d4f47a1ab6528bd99725f345a78d9
Florian Schwennsen <florian.schwennsen@desy.de>
2024-01-17 15:35:54 +0100

    theses-saskatchewan3: new DSpace version

--- active/theses-saskatchewan3.py
+++ active/theses-saskatchewan3.py
@@ -2,11 +2,13 @@
 #harvest theses from Saskatchewan U.
 #FS: 2020-09-25
 #FS: 2023-04-17
+#FS: 2024-01-17
 
 import getopt
 import sys
 import os
-import urllib.request, urllib.error, urllib.parse
+#import urllib.request, urllib.error, urllib.parse
+import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
 import re
 import ejlmod3
@@ -17,7 +19,7 @@ publisher = 'Saskatchewan U.'
 jnlfilename = 'THESES-SASKATCHEWAN-%s' % (ejlmod3.stampoftoday())
 
 rpp = 50
-pages = 5
+pages = 10
 skipalreadyharvested = False
 boring = ['Chemistry', 'Veterinary Biomedical Sciences', 'English', 'Educational Foundations',
           'History', 'School of Environment and Sustainability', 'Soil Science',
@@ -28,97 +30,73 @@ boring = ['Chemistry', 'Veterinary Biomedical Sciences', 'English', 'Educational
           'Johnson-Shoyama Graduate School of Public Policy', 'Nursing', 'Pharmacology',
           'School of Public Health', 'Veterinary Pathology', 'Toxicology Centre',
           'Animal and Poultry Science', 'Biology', 'Civil and Geological Engineering',
-          'Community Health and Epidemiology', 'Geography and Planning',
+          'Community Health and Epidemiology', 'Geography and Planning', 'Geography',
           'Large Animal Clinical Sciences', 'Law', 'Mechanical Engineering', 'Medicine',
           'Microbiology and Immunology', 'Psychology', 'Sociology', 'Veterinary Microbiology',
           'Anatomy and Cell Biology', 'Art and Art History', 'Biochemistry', 'Curriculum Studies',
           'Food and Bioproduct Sciences', 'Geological Sciences', 'Kinesiology',
           'Bioresource Policy, Business and Economics', 'Environmental Engineering',
           'Nutrition', 'Pathology and Laboratory Medicine', 'Western College of Veterinary Medicine',
-          'Pharmacy and Nutrition', 'Physiology', 'Plant Sciences', 'School of Physical Therapy']
+          'Pharmacy and Nutrition', 'Physiology', 'Plant Sciences', 'School of Physical Therapy',
+          'Anatomy, Physiology, and Pharmacology', 'Interdisciplinary Food Science Program',
+          'Agricultural and Bioresource Engineering', 'Agricultural Economics',
+          'Agricultural Engineering', 'Agriculture and Bioresource Engineering', 'Animal Husbandry',
+          'Animal Science', 'Applied Microbiology and Food Science', 'Cancer Research',
+          'Chemical Engineering', 'Chemistry and Chemical Engineering', 'Civil Engineering',
+          'College of Agriculture', 'College of Arts and Science', 'College of Education',
+          'College of Graduate Studies', 'College of Kinesiology', 'College of Pharmacy and Nutrition',
+          'Crop Science and Plant Ecology', 'Crop Science', 'Dairy Science', 'Economics',
+          'Educational Psychology and Special Education', 'Field Husbandry', 'Finance', 'Food Science',
+          'Horticulture', 'Linguistics', 'Native Studies', 'Pharmacy', 'Physiology and Pharmacology',
+          'Plant Ecology', 'Plant Science', 'Political Science', 'Political Studies',
+          'Poultry Science', 'Psychiatry', 'Toxicology']
 boring += ['Master of Science (M.Sc.)', 'Master of Education (M.Ed.)', 'Master of Arts (M.A.)',
            'Master of Nursing (M.N.)', 'Master of Public Policy (M.P.P.)',
            'Master of Laws (LL.M.)', 'Master of Fine Arts (M.F.A.)']
-boringdegrees = []
-for b in boring:
-    boringdegrees.append(re.sub(' ', '+', re.sub('\(', '%28', re.sub('\)', '%29', b))))
+
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
 
 if skipalreadyharvested:
     alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
 else:
     alreadyharvested = []
 
-
 hdr = {'User-Agent' : 'Magic Browser'}
-prerecs = []
-for page in range(pages):
-    tocurl = 'https://harvest.usask.ca/handle/10388/381/discover?sort_by=dc.date.issued_dt&order=desc&rpp=' + str(rpp) + '&page=' + str(page+1)
-    ejlmod3.printprogress("=", [[page+1, pages], [tocurl]])
-    req = urllib.request.Request(tocurl, headers=hdr)
-    tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-    prerecs += ejlmod3.getdspacerecs(tocpage, 'https://harvest.usask.ca', alreadyharvested=alreadyharvested, boringdegrees=boringdegrees)
-    print('  %4i records so far' % (len(prerecs)))
 
-i = 0
+
 recs = []
-for rec in prerecs:
-    i += 1
-    keepit = True
-    ejlmod3.printprogress("-", [[i, len(prerecs)], [rec['link']], [len(recs)]])
-    try:
-        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-        time.sleep(3)
-    except:
-        try:
-            print("retry %s in 180 seconds" % (rec['link']))
-            time.sleep(180)
-            artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-        except:
-            print("no access to %s" % (rec['link']))
-            continue
-    ejlmod3.metatagcheck(rec, artpage, ['DC.title', 'DCTERMS.issued', 'DC.subject',
-                                        'DCTERMS.abstract', 'citation_pdf_url'])
-    for meta in artpage.head.find_all('meta'):
-        if meta.has_attr('name'):
-            #author
-            if meta['name'] == 'DC.creator':
-                if re.search('\d{4}\-\d{4}\-', meta['content']):
-                    rec['autaff'][-1].append('ORCID:' + meta['content'])
-                else:
-                    rec['autaff'] = [[  meta['content'] ]]
-    rec['autaff'][-1].append(publisher)
-    for div in artpage.body.find_all('div', attrs = {'class' : 'table'}):
-        for h5 in div.find_all('h5'):
-            h5t = h5.text.strip()
-            h5.decompose()
-            #degree
-            if h5t == 'Degree':
-                rec['degree'] = div.text.strip()
-                rec['note'].append(rec['degree'])
-                if rec['degree'] in boring:
-                    print('   skip "%s"' % (rec['degree']))
-                    keepit = False
-            #Department
-            elif h5t == 'Department':
-                rec['department'] = div.text.strip()
-                rec['note'].append(rec['department'])
-                if rec['department'] in boring:
-                    print('   skip "%s"' % (rec['department']))
-                    keepit = False
-            #supervisor
-            elif h5t == 'Supervisor':
-                for span in div.find_all('span'):
-                    for sv in re.split('; ', span.text.strip()):
-                        if re.search('@', sv):
-                            email = re.sub('.* (.*?@[\w\.]+).*', r'EMAIL:\1', sv)
-                            sv = re.sub(' .*?@.*', '', sv)
-                            rec['supervisor'].append([sv, email])
-                        else:
-                            rec['supervisor'].append([sv])
-    if keepit:
-        ejlmod3.printrecsummary(rec)
-        recs.append(rec)
-    else:
-        ejlmod3.adduninterestingDOI(rec['hdl'])
+allhdls = []
+baseurl = 'https://harvest.usask.ca'
 
+recs = []
+for page in range(pages):
+    tocurl = baseurl + '/collections/c58530ca-7de1-416a-81bc-4b715d1ddf2e?cp.page=' + str(page+1) + '&cp.rpp=' + str(rpp)
+    ejlmod3.printprogress('=', [[page+1, pages], [tocurl]])
+    try:
+        driver.get(tocurl)
+        time.sleep(5)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    except:
+        time.sleep(60)
+        driver.get(tocurl)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    
+    for rec in ejlmod3.ngrx(tocpage, baseurl, ['dc.contributor.advisor', 'dc.creator',  
+                                               'dc.date.issued', 'thesis.degree.department',
+                                               'dc.degree.discipline', 'dc.language.iso', 
+                                               'dc.rights', 'thesis.degree.level', 'thesis.degree.name', 
+                                               'dc.description.abstract', 'dc.identifier.uri',
+                                               'dc.subject', 'dc.title'],
+                            boring=boring, alreadyharvested=alreadyharvested):
+        rec['autaff'][-1].append(publisher)
+        ejlmod3.printrecsummary(rec)
+        #print(rec['thesis.metadata.keys'])
+        recs.append(rec)
+    print('  %i records so far' % (len(recs)))
+    time.sleep(20)
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
+
+
