38d3df058973893d51289e0cf152246db522422b
Florian Schwennsen <florian.schwennsen@desy.de>
2024-03-12 14:06:43 +0100

    theses-carleton3: major rewrite

--- active/theses-carleton3.py
+++ active/theses-carleton3.py
@@ -2,14 +2,35 @@
 #harvest theses from Carleton U. (main)
 #FS: 2019-12-12
 #FS: 2023-04-07
+#FS: 2024-03-07
 
 import sys
 import os
 import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 import re
+import undetected_chromedriver as uc
 import ejlmod3
 import time
+
+rpp = 50 
+pages = 15
+skipalreadyharvested = True
+boring = ['Geography', 'Engineering, Environmental', 'Architecture',
+          'Digital Media', 'Applied Linguistics and Discourse Studies', 'Biology',
+          'Earth Sciences', 'Engineering, Building', 'Engineering, Civil',
+          'Management', 'Political Science', 'Psychology', 'Public Policy',
+          'Sociology', 'Anthropology', 'Cognitive Science', 'Economics',
+          'English', 'Neuroscience', 'Canadian Studies', 'Chemistry',
+          'Communication', 'Cultural Mediations', 'Engineering, Aerospace',
+          'Ethics and Public Affairs', 'Health Sciences', 'International Affairs',
+          'Legal Studies', 'Social Work', 'DIS:::Engineering, Biomedical',
+          'History']
+boring += ["Master's"]
+baseurl = 'https://repository.library.carleton.ca'
+collection = 'm039k491c'
+tags = ['dc.contributor.author', 'dc.title']
+
 
 publisher = 'Carleton U. (main)'
 jnlfilename = 'THESES-CARLETON-%s' % (ejlmod3.stampoftoday())
@@ -17,55 +38,111 @@ skipalreadyharvested = True
 
 if skipalreadyharvested:
     alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
 
-hdr = {'User-Agent' : 'Magic Browser'}
+
+
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
+
 prerecs = []
-for discipline in ['Physics', 'Mathematics', 'FLAG']:
-    for year in [str(ejlmod3.year()), str(ejlmod3.year(backwards=1))]:
-        tocurl = 'https://curve.carleton.ca/167299e9-53e6-48d7-a28d-8af2f87719ec?f%5B0%5D=thesis_degree_level%3ADoctoral&f%5B1%5D=dcterms_date%3A' + year + '&f%5B2%5D=thesis_degree_discipline%3A' + discipline
-        ejlmod3.printprogress('=', [[discipline], [year], [tocurl]])
-        req = urllib.request.Request(tocurl, headers=hdr)
-        tocpage = BeautifulSoup(urllib.request.urlopen(req), features='lxml')
-        divs = tocpage.body.find_all('div', attrs = {'class' : 'views-row'})
-        for div in divs:
-            rec = {'tc' : 'T', 'keyw' : [], 'jnl' : 'BOOK'}
-            if discipline == 'Mathematics':
-                rec['fc'] = 'm'
-            for div2 in div.find_all('div', attrs = {'class' : 'views-field-title'}):
-                for a in div2.find_all('a'):
-                    rec['link'] = 'https://curve.carleton.ca' + a['href']
-                    prerecs.append(rec)
-        print('  %4i records so far' % (len(prerecs)))
+for page in range(pages):
+    tocurl = baseurl + '/collections/' + collection + '?page=' + str(page+1) + '&per_page=' + str(rpp) + '&sort=system_create_dtsi+desc'
+    ejlmod3.printprogress('=', [[page+1, pages], [tocurl]])
+    try:
+        driver.get(tocurl)
         time.sleep(5)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    except:
+        time.sleep(60)
+        driver.get(tocurl)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    for tr in tocpage.find_all('tr'):
+        for p in tr.find_all('p', attrs = {'class' : 'media-heading'}):
+            rec = {'jnl' : 'BOOK', 'tc' : 'T', 'note' : [], 'keyw' : []}
+            for a in p.find_all('a'):
+                rec['artlink'] = baseurl + a['href']
+                if ejlmod3.checkinterestingDOI(rec['artlink']):
+                    prerecs.append(rec)
+                else:
+                    print('  ', rec['artlink'], 'is uninteresting')
+    print('  %i records so far' % (len(prerecs)))
+    time.sleep(20)
 
-i = 0
+
 recs = []
-for rec in prerecs:
-        rec['doi'] = '20.2000/' + re.sub('\W', '', rec['link'][6:])
-        i += 1
-        ejlmod3.printprogress("-", [[i, len(prerecs)], [rec['link']], [len(recs)]])
-        try:
-            artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features='lxml')
-            time.sleep(3)
-        except:
+for (i, rec) in enumerate(prerecs):
+    keepit = True
+    ejlmod3.printprogress('-', [[i+1, len(prerecs)], [rec['artlink']], [len(recs)]])
+    try:
+        driver.get(rec['artlink'])
+        time.sleep(5)
+        artpage = BeautifulSoup(driver.page_source, features="lxml")
+    except:
+        time.sleep(60)
+        driver.get(rec['artlink'])
+        artpage = BeautifulSoup(driver.page_source, features="lxml")
+    ejlmod3.metatagcheck(rec, artpage, ['citation_pdf_url', 'citation_title', 'citation_author',
+                                        'citation_publication_date'])
+    for dl in artpage.find_all('dl'):
+        for child in dl.children:
             try:
-                print("retry %s in 180 seconds" % (rec['link']))
-                time.sleep(180)
-                artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features='lxml')
+                dn = child.name
             except:
-                print("no access to %s" % (rec['link']))
-                continue    
-        ejlmod3.metatagcheck(rec, artpage, ['dcterms.creator', 'citation_title', 'dcterms.date',
-                                            'abstract', 'citation_pdf_url'])
-        rec['autaff'][-1].append(publisher)
-        #license
-        ejlmod3.globallicensesearch(rec, artpage)
-        #doi
-        for a in artpage.find_all('a'):
-            if a.has_attr('href') and re.search('doi.org\/10\.22215', a['href']):
-                rec['doi'] = re.sub('.*doi.org\/', '', a['href'])
-        if not skipalreadyharvested or not rec['doi'] in alreadyharvested:
-            ejlmod3.printrecsummary(rec)
+                dn = ''
+            if dn == 'dt':
+                dt = child.text.strip()
+            elif dn == 'dd':
+                #abstract
+                if dt == 'Abstract':
+                    rec['abs'] = child.text.strip()
+                #subject
+                if dt == 'Subject':
+                    for a in child.find_all('a'):
+                        rec['keyw'].append(a.text.strip())
+                #Thesis Degree Level
+                if dt == 'Thesis Degree Level':
+                    degree = child.text.strip()
+                    if degree in boring:
+                        print('  skip "%s"' % (degree))
+                        keepit = False                        
+                    if degree != 'Doctoral':
+                        rec['note'].append('DEG:::' + degree)
+                #DOI
+                if dt == 'Identifier':
+                    for a in child.find_all('a'):
+                        rec['doi'] = re.sub('.*doi.org\/', '', a['href'])
+                #discipline
+                if dt == 'Thesis Degree Discipline' and keepit:
+                    discipline = child.text.strip()
+                    if discipline in boring:
+                        print('  skip "%s"' % (discipline))
+                        keepit = False
+                    elif discipline in ['Pure Mathematics']:
+                        rec['fc'] = 'm'
+                    elif discipline in ['Computer Science']:
+                        rec['fc'] = 'c'
+                    elif not discipline in ['Physics']:
+                        rec['note'].append('DIS:::' + discipline)
+    if keepit:
+        if skipalreadyharvested and 'doi' in rec and rec['doi'] in alreadyharvested:
+            print('  already in backup')
+        else:
             recs.append(rec)
+            ejlmod3.printrecsummary(rec)
+    else:
+        ejlmod3.adduninterestingDOI(rec['artlink'])
+        
 
+    
+    
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
+
+
+
+
+
+        
