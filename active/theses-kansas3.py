e3902f88d5dc04f545c4f94541c66447aaa64caf
Florian Schwennsen <florian.schwennsen@desy.de>
2024-04-08 11:59:14 +0200

    theses-kansas3: new DSpace version

--- active/theses-kansas3.py
+++ active/theses-kansas3.py
@@ -2,90 +2,85 @@
 #harvest theses from Kansas State U.
 #FS: 2020-02-10
 #FS: 2023-02-20
+#FS: 2024-04-08
 
 import getopt
 import sys
 import os
-import urllib.request, urllib.error, urllib.parse
+import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
 import re
 import ejlmod3
 import time
 
 publisher = 'Kansas State U.'
-pages = 40+60
+pages = 5
+rpp = 100 
 skipalreadyharvested = True
-
-rejectwords = []
-for rejectword in ['Department of Adult Learning', 'Department of Agricultural', 'Department of Agronomy',
-                   'Department of Anatomy', 'Department of Animal', 'Department of Apparel', 'Department of Bio',
-                   'Department of Chemi', 'Department of Civil Engineering', 'Department of Communications Studies',
-                   'Department of Curriculum and Instruction', 'Department of Diagnostic',
-                   'Department of Diagnostic Medicine and Pathobiology', 'Department of Economics',
-                   'Department of Educational', 'Department of Entomology', 'Department of Environmental Design',
-                   'Department of Family', 'Department of Food', 'Department of Geography',
-                   'Department of Grain Science', 'Department of History', 'Department of Horticulture',
-                   'Department of Hospitality Managemen', 'Department of Human', 'Department of Industrial',
-                   'Department of Kinesiology', 'Department of Landscape', 'Department of Mechanical',
-                   'Department of Modern Languages', 'Department of Music', 'Department of Plant',
-                   'Department of Plant Pathology', 'Department of Psycho', 'Department of Psychological',
-                   'Department of Soci', 'Department of Sociology', 'Department of Special Education',
-                   'Department of Statistics', 'Kansas Department of Transportation', 'Master of ']:
-    rejectwords.append(re.compile(rejectword))
+boring = ['Biochemistry and Molecular Biophysics Interdepartmental Program', 'Curriculum and Instruction Programs',
+          'Department of Agricultural Economics', 'Department of Agronomy',
+          'Department of Animal Sciences and Industry', 'Department of Architectural Engineering and Construction Science',
+          'Department of Architectural Engineering', 'Department of Architecture',
+          'Department of Biological &a; Agricultural Engineering', 'Department of Biology', 'Department of Chemical Engineering',
+          'Department of Chemistry', 'Department of Clinical Sciences', 'Department of Diagnostic Medicine/Pathobiology',
+          'Department of Economics', 'Department of Educational Leadership', 'Department of Electrical and Computer Engineering',
+          'Department of Entomology', 'Department of Food, Nutrition, Dietetics and Health', 'Department of Geography',
+          'Department of Geology', 'Department of Grain Science and Industry', 'Department of History',
+          'Department of Horticulture and Natural Resources', 'Department of Human Ecology-Personal Financial Planning',
+          'Department of Journalism and Mass Communications', 'Department of Kinesiology',
+          'Department of Landscape Architecture/Regional and Community Planning',
+          'Department of Mechanical and Nuclear Engineering', 'Department of Psychological Sciences',
+          'Department of Sociology, Anthropology, and Social Work', 'Food Science Institute',
+          'Genetics Interdepartmental Program',
+          'Public Health Interdepartmental Program', 'Department of Anatomy and Physiology',
+          'Department of Civil Engineering', 'Division of Biology']
 
 jnlfilename = 'THESES-KANSASSTATE-%s' % (ejlmod3.stampoftoday())
 
 if skipalreadyharvested:
     alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
 
-prerecs = []
-hdr = {'User-Agent' : 'Magic Browser'}
-for page in range(pages):
-    tocurl = 'https://krex.k-state.edu/dspace/handle/2097/4/recent-submissions?offset=' + str(page*5)
-    ejlmod3.printprogress("=", [[page+1, pages], [tocurl]])
-    req = urllib.request.Request(tocurl, headers=hdr)
-    tocpage = BeautifulSoup(urllib.request.urlopen(req), features="lxml")
-    time.sleep(5)
-    for rec in ejlmod3.getdspacerecs(tocpage, 'https://krex.k-state.edu'):
-        if skipalreadyharvested and rec['hdl'] in alreadyharvested:
-            pass
-        else:
-            prerecs.append(rec)
-    print('  %4i records so far' % (len(prerecs)))
+options = uc.ChromeOptions()
+options.binary_location='/usr/bin/chromium'
+chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
+driver = uc.Chrome(version_main=chromeversion, options=options)
 
-i = 0
+
+baseurl = 'https://krex.k-state.edu'
+collection = '6d684812-6d29-4564-8b51-f5670957936b'
+
 recs = []
-for rec in prerecs:
-    interesting = True
-    i += 1
-    ejlmod3.printprogress("-", [[i, len(prerecs)], [rec['link']], [len(recs)]])
+for page in range(pages):
+    tocurl = baseurl + '/collections/' + collection + '?cp.page=' + str(page+1) + '&cp.rpp=' + str(rpp)
+    ejlmod3.printprogress('=', [[page+1, pages], [tocurl]])
     try:
-        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-        time.sleep(5)
+        driver.get(tocurl)
+        time.sleep(10)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
     except:
-        try:
-            print("retry %s in 180 seconds" % (rec['link']))
-            time.sleep(180)
-            artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-        except:
-            print("no access to %s" % (rec['link']))
-            continue
-    keepit = True
-    ejlmod3.metatagcheck(rec, artpage, ['citation_author', 'citation_pdf_url', 'DCTERMS.abstract',
-                                        'citation_title', 'citation_date', 'citation_keywords'])
-    #thesis type
-    for meta in artpage.head.find_all('meta', attrs = {'name' : 'DC.description'}):
-        rec['note'].append(meta['content'])
-        if keepit:
-            for rejectword in rejectwords:
-                if rejectword.search(meta['content']):
-                    keepit = False
-                    break
-    if keepit:
+        time.sleep(60)
+        driver.get(tocurl)
+        time.sleep(10)
+        tocpage = BeautifulSoup(driver.page_source, features="lxml")
+    for rec in ejlmod3.ngrx(tocpage, baseurl, ['dc.contributor.author',  'dc.date.published',
+                                               'dc.description.abstract', 'dc.description.advisor',
+                                               'dc.description.level',  'dc.identifier.uri',
+                                               'dc.description.department', 'dc.subject', 'dc.title'],
+                            boring=boring, alreadyharvested=alreadyharvested):        
         rec['autaff'][-1].append(publisher)
+        #print(rec['thesis.metadata.keys'])
         recs.append(rec)
-        ejlmod3.printrecsummary(rec)
-    else:
-        ejlmod3.adduninterestingDOI(rec['hdl'])
-
+    print('  %i records so far' % (len(recs)))
+    time.sleep(20)
 ejlmod3.writenewXML(recs, publisher, jnlfilename)
+
+
+
+
+
+
+
+
+
