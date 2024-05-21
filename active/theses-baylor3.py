0ae452f700f0ee7c57fa9b8af2c0c4ffb81eb71e
Florian Schwennsen <florian.schwennsen@desy.de>
2024-05-21 12:14:24 +0200

    theses-baylor3: use google-chrome (runs on inspire4 again)

--- active/theses-baylor3.py
+++ active/theses-baylor3.py
@@ -44,6 +44,8 @@ boring = ['Baylor University. Dept. of Biology.', 'Baylor University. Dept. of C
 
 options = uc.ChromeOptions()
 options.binary_location='/usr/bin/chromium'
+options.binary_location='/usr/bin/google-chrome'
+options.add_argument('--headless')
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
 
