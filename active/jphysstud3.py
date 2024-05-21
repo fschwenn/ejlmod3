14350a3fc58728ea67ce97e20b90fa8dda74295b
Florian Schwennsen <florian.schwennsen@desy.de>
2024-05-21 12:17:55 +0200

    jphysstud3: use google-chrome (runs on inspire4 again)

--- active/jphysstud3.py
+++ active/jphysstud3.py
@@ -32,7 +32,7 @@ print("get table of content... from %s" % (toclink))
 
 options = uc.ChromeOptions()
 options.binary_location='/usr/bin/google-chrome'
-options.binary_location='/usr/bin/chromium'
+#options.binary_location='/usr/bin/chromium'
 options.add_argument('--headless')
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
