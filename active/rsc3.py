5239414bdba016ee125bdc488b52a74f94172722
Florian Schwennsen <florian.schwennsen@desy.de>
2024-05-21 12:17:04 +0200

    rsc3: use google-chrome (runs on inspire4 again)

--- active/rsc3.py
+++ active/rsc3.py
@@ -29,7 +29,9 @@ tocurl = 'https://pubs.rsc.org/en/journals/journalissues/' + journal + '#!issuei
 print(tocurl)
 
 options = uc.ChromeOptions()
-options.binary_location='/usr/bin/chromium'
+#options.binary_location='/usr/bin/chromium'
+options.add_argument('--headless')
+options.binary_location='/usr/bin/google-chrome'
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
 
