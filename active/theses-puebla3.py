807bf8905edbbb433190ea55d55ff02c912f0258
Florian Schwennsen <florian.schwennsen@desy.de>
2024-05-22 13:21:22 +0200

    theses-puebla3: use google-chrome (on inspire4 again)

--- active/theses-puebla3.py
+++ active/theses-puebla3.py
@@ -22,7 +22,7 @@ import time
 options = uc.ChromeOptions()
 options.headless=True
 options.binary_location='/usr/bin/google-chrome'
-options.binary_location='/usr/bin/chromium'
+#options.binary_location='/usr/bin/chromium'
 options.add_argument('--headless')
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
