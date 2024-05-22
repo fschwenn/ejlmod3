b30def3f751893d7b5f78c69b63ca1367e8a64d2
Florian Schwennsen <florian.schwennsen@desy.de>
2024-05-22 13:22:28 +0200

    theses-zaragoza3: use google-chrome (on inspire4 again)

--- active/theses-zaragoza3.py
+++ active/theses-zaragoza3.py
@@ -114,7 +114,7 @@ if skipalreadyharvested:
 # Initialize Webdriver
 options = uc.ChromeOptions()
 options.binary_location='/opt/google/chrome/google-chrome'
-options.binary_location='/usr/bin/chromium'
+#options.binary_location='/usr/bin/chromium'
 options.add_argument('--headless')
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
