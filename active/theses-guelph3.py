8937013747f520ea8bef555021e8c5a7f9b499e9
Florian Schwennsen <florian.schwennsen@desy.de>
2024-05-22 13:22:59 +0200

    theses-guelph3: use google-chrome (on inspire4 again)

--- active/theses-guelph3.py
+++ active/theses-guelph3.py
@@ -65,6 +65,8 @@ else:
 
 options = uc.ChromeOptions()
 options.binary_location='/usr/bin/chromium'
+options.binary_location='/usr/bin/google-chrome'
+options.add_argument('--headless')
 chromeversion = int(re.sub('.*?(\d+).*', r'\1', os.popen('%s --version' % (options.binary_location)).read().strip()))
 driver = uc.Chrome(version_main=chromeversion, options=options)
 
