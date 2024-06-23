9c72d99589eb27cd4ebf4fe3271cc47417395530
Florian Schwennsen <florian.schwennsen@desy.de>
2024-06-23 18:34:43 +0200

    theses-trinity3: major rewrite

--- active/theses-trinity3.py
+++ active/theses-trinity3.py
@@ -12,11 +12,10 @@ import re
 import ejlmod3
 import time
 
-years = [ejlmod3.year(backwards=1), ejlmod3.year()]
-rpp = 50
-pages = 20
-numberofrecords = rpp*pages
+rpp = 20 # fix
+pages = 5
 skipalreadyharvested = True
+years = 2
 
 publisher = 'TCD, Dublin'
 jnlfilename = 'THESES-TrinityCollegeDublin-%s' % (ejlmod3.stampoftoday())
@@ -35,34 +34,26 @@ if skipalreadyharvested:
 hdls = []
 
 prerecs = []
-for year in years:
-    for section in ['76240', '221', '171']:
-        realpages = pages
-        for page in range(pages):
-            if page*rpp < numberofrecords:
-                tocurl = 'http://www.tara.tcd.ie/handle/2262/' + section + '/discover?rpp=' + str(rpp) + '&page=' + str(page+1) + '&group_by=none&etal=0&filtertype_0=dateIssued&filtertype_1=type&filter_0=[' + str(year) + '+TO+' + str(year) + ']&filter_relational_operator_1=equals&filter_1=Thesis&filter_relational_operator_0=equals'
-                ejlmod3.printprogress("=", [[year], [section], [page+1, realpages]])
-                try:
-                    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl), features="lxml")
-                    time.sleep(3)
-                except:
-                    print("retry %s in 180 seconds" % (tocurl))
-                    time.sleep(180)
-                    tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl), features="lxml")
-                if page == 0:
-                    for p in tocpage.body.find_all('p', attrs = {'class' : 'pagination-info'}):
-                        if re.search('\d of \d+', p.text):
-                            numberofrecords = int(re.sub('.*of (\d+).*', r'\1', p.text.strip()))
-                            print('  %i theses in query' % (numberofrecords))
-                            realpages = (numberofrecords-1) // rpp + 1
-                for rec in ejlmod3.getdspacerecs(tocpage, 'http://www.tara.tcd.ie'):
-                    if rec['hdl'] in hdls or rec['hdl'] in ['2262/82940']:
-                        print('  HDL:%s already from other collection' % (rec['hdl']))
-                    elif skipalreadyharvested and rec['hdl'] in alreadyharvested:
-                        print('  HDL:%s already in backup' % (rec['hdl']))                        
-                    else:
-                        prerecs.append(rec)
-                        hdls.append(rec['hdl'])
+for section in ['76240', '221', '171']:
+    for page in range(pages):
+        #tocurl = 'http://www.tara.tcd.ie/handle/2262/' + section + '/discover?rpp=' + str(rpp) + '&page=' + str(page+1) + '&group_by=none&etal=0&filtertype_0=dateIssued&filtertype_1=type&filter_0=[' + str(year) + '+TO+' + str(year) + ']&filter_relational_operator_1=equals&filter_1=Thesis&filter_relational_operator_0=equals'
+        tocurl = 'https://www.tara.tcd.ie/handle/2262/' + section + '/recent-submissions?offset=' + str(rpp*page)
+        ejlmod3.printprogress("=", [[section], [page+1, pages], [tocurl]])
+        try:
+            tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl), features="lxml")
+            time.sleep(3)
+        except:
+            print("retry %s in 180 seconds" % (tocurl))
+            time.sleep(180)
+            tocpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(tocurl), features="lxml")
+        for rec in ejlmod3.getdspacerecs(tocpage, 'http://www.tara.tcd.ie'):
+            if rec['hdl'] in hdls or rec['hdl'] in ['2262/82940']:
+                print('  HDL:%s already from other collection' % (rec['hdl']))
+            elif skipalreadyharvested and rec['hdl'] in alreadyharvested:
+                print('  HDL:%s already in backup' % (rec['hdl']))                        
+            else:
+                prerecs.append(rec)
+                hdls.append(rec['hdl'])
         print('  %4i records so far' % (len(prerecs)))
     
 i = 0
@@ -70,19 +61,29 @@ recs = []
 for rec in prerecs:
     keepit = True
     i += 1
-    ejlmod3.printprogress("-", [[year], [i, len(prerecs)], [rec['link']], [len(recs)]])
+    ejlmod3.printprogress("-", [[i, len(prerecs)], [rec['link']], [len(recs)]])
+    try:
+        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
+        time.sleep(3)
+    except:
+        print("retry %s in 180 seconds" % (rec['link'] + '?show=full'))
+        time.sleep(180)
+        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
+    #license
+    ejlmod3.globallicensesearch(rec, artpage)
     try:
         artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link'] + '?show=full'), features="lxml")
         time.sleep(3)
     except:
         print("retry %s in 180 seconds" % (rec['link'] + '?show=full'))
         time.sleep(180)
-        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link']), features="lxml")
-    ejlmod3.metatagcheck(rec, artpage, ['citation_authors', 'DCTERMS.issued', 'DCTERMS.abstract',
-                                        'DC.subject'])
+        artpage = BeautifulSoup(urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(rec['link'] + '?show=full'), features="lxml")
+    ejlmod3.metatagcheck(rec, artpage, ['citation_author', 'DCTERMS.issued', 'DCTERMS.abstract',
+                                        'DC.subject', 'citation_pdf_url'])
     for meta in artpage.head.find_all('meta', attrs = {'name' : 'DC.publisher'}):
-        if re.search('School of', meta['content']):
-            rec['school'] = re.sub('.*School of (.*?)\..*', r'\1', meta['content'])
+        if re.search('(Department|School) of', meta['content']):
+            rec['school'] = re.sub('.*(Department|School) of (.*?)\..*', r'\2', meta['content'])
+            rec['school'] = re.sub('.*(Department|School) of (.*)', r'\2', rec['school'])
         else:
             rec['note'].append(meta['content'])
     if 'school' in list(rec.keys()):
@@ -92,12 +93,10 @@ for rec in prerecs:
         elif rec['school'] in boringschools:
             print('   skip School of %s' % (rec['school']))
             keepit = False
-        else:
+        elif not rec['school'] in ['Physics', 'Computer Science & Statistics']:
             rec['note'].append(rec['school'])
     if len(rec['autaff'][-1]) == 1:
         rec['autaff'][-1].append(publisher)
-    #license
-    ejlmod3.globallicensesearch(rec, artpage)
     #fulltext
     for div in artpage.body.find_all('div', attrs = {'class' : 'simple-item-view-download'}):
         for a in div.find_all('a'):
@@ -108,16 +107,33 @@ for rec in prerecs:
     #supervisor
     for tr in artpage.body.find_all('tr', attrs = {'class' : 'ds-table-row'}):
         tdt = ''
+        #print('\ntr: ', tr.text.strip())
         for td in tr.find_all('td'):
-            if td.has_attr('label-cell'):
+            #print('td: ', td.text.strip())
+            if td.has_attr('class') and 'label-cell' in td['class']:
                 tdt = td.text.strip()
+                #print('tdt:', tdt)
             else:
                 if tdt == 'dc.contributor.advisor':
-                    rec['supervisor'].append(td.text.strip())
-                
+                    tdtt = td.text.strip()
+                    if len(tdtt) > 3:
+                        rec['supervisor'].append([td.text.strip()])
+                        #print('\033[93msv: ', td.text.strip(), '\033[0m')
+    #degree level
+                elif tdt == 'dc.type.qualificationlevel':
+                    degreelevel = td.text.strip()
+                    if degreelevel in ['Masters']:
+                        keepit = False
+                        print('  skip', degreelevel)
+                    elif not degreelevel in ['Doctoral', 'en']:
+                        rec['note'].append('DEG:::' + degreelevel)    
+    
     if keepit:
-        recs.append(rec)
-        ejlmod3.printrecsummary(rec)
+        if 'year' in rec and int(rec['year']) <= ejlmod3.year(backwards=years):
+            print('   %s too old' % (rec['year']))
+        else:
+            recs.append(rec)
+            ejlmod3.printrecsummary(rec)
     else:
         ejlmod3.adduninterestingDOI(rec['hdl'])
 
