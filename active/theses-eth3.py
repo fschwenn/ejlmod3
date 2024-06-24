e33acc185e0dfed1b22a8dd9eb5d67895a11f52c
Florian Schwennsen <florian.schwennsen@desy.de>
2024-06-24 21:47:04 +0200

    theses-eth3: use ejlmod3.getalreadyharvested()

--- active/theses-eth3.py
+++ active/theses-eth3.py
@@ -18,15 +18,12 @@ publisher = 'Zurich, ETH'
 jnlfilename = 'THESES-ETH-%s' % (ejlmod3.stampoftoday())
 rpp = 100
 skipalreadyharvested = True
-dokidir = '/afs/desy.de/user/l/library/dok/ejl/backup'
 
-
-alreadyharvested = []
-def tfstrip(x): return x.strip()
 if skipalreadyharvested:
-    filenametrunc = re.sub('\d.*', '*doki', jnlfilename)
-    alreadyharvested = list(map(tfstrip, os.popen("cat %s/*%s %s/%i/*%s | grep URLDOC | sed 's/.*=//' | sed 's/;//' " % (dokidir, filenametrunc, dokidir, ejlmod3.year(backwards=1), filenametrunc))))
-    print('%i records in backup' % (len(alreadyharvested)))        
+    alreadyharvested = ejlmod3.getalreadyharvested(jnlfilename)
+else:
+    alreadyharvested = []
+
 
 hdr = {'User-Agent' : 'Magic Browser'}
 prerecs = []
