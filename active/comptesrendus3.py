1a9af15afb49a4c3ab9f3431fa8061002e44bc1c
Florian Schwennsen <florian.schwennsen@desy.de>
2024-06-26 19:49:46 +0200

    comptesrendus3: added 'Alg.Comb.' (plus aff extraction and improved reference extraction)

--- active/comptesrendus3.py
+++ active/comptesrendus3.py
@@ -51,6 +51,11 @@ elif jnl == 'physique':
         tocurl = 'https://comptes-rendus.academie-sciences.fr/physique/volume/CRPHYS_%s__%s' % (year, vol)
         tocurl = 'https://comptes-rendus.academie-sciences.fr/physique/volume_general/CRPHYS_%s__%s' % (year, vol)
     jnlname = 'Comptes Rendus Physique'
+elif jnl == 'algcomb':
+    year = str(2017 + int(vol))
+    tocurl = 'https://alco.centre-mersenne.org/issues/ALCO_%s__%s_%s/' % (year, vol, iss)
+    jnlname = 'Alg.Comb.'
+    publisher = 'TCC / Centre Mersenne'
 
 hdr = {'User-Agent' : 'Mozilla/5.0'}
 try:
@@ -75,7 +80,10 @@ for span in tocpage.body.find_all('span', attrs = {'class' : 'article-title'}):
                'year' : year, 'vol' : vol, 'refs' : []}
         if iss:
             rec['issue'] = iss
-        rec['artlink'] = 'https://comptes-rendus.academie-sciences.fr' + a['href']
+        if jnl == 'algcomb':
+            rec['artlink'] = 'https://alco.centre-mersenne.org' + a['href']
+        else:
+            rec['artlink'] = 'https://comptes-rendus.academie-sciences.fr' + a['href']
         prerecs.append(rec)
 
 i = 0
@@ -92,13 +100,26 @@ for rec in prerecs:
     for div in artpage.body.find_all('div', attrs = {'id' : 'abstract-en'}):
         for p in div.find_all('p'):
             rec['abs'] = p.text.strip()
+    #affiliations
+    affdict = {}
+    for div in artpage.body.find_all('div', attrs = {'class' : 'affiliation-authors'}):
+        for span in div.find_all('span'):
+            if span.has_attr('id'):
+                for sup in span.find_all('sup'):
+                    affkey = sup.text.strip()
+                    sup.decompose()
+                affdict[affkey] = span.text.strip()
     #authors
     for div in artpage.body.find_all('div', attrs = {'class' : 'article-author'}):
         for a in div.find_all('a'):
             if re.search('orcid.org', a['href']):
                 rec['autaff'][-1].append(re.sub('.*\/', 'ORCID:', a['href']))
             else:
-                rec['autaff'].append([a.text.strip()])
+                ats = a.text.strip()
+                if len(ats) > 2:
+                    rec['autaff'].append([a.text.strip()])
+                elif ats in affdict:
+                    rec['autaff'][-1].append(affdict[ats])
     #date
     for div in artpage.body.find_all('div', attrs = {'id' : 'info-tab'}):
         divt = re.sub('[\n\t\r]', ' ', div.text.strip())
@@ -108,15 +129,21 @@ for rec in prerecs:
     for div in artpage.body.find_all('div', attrs = {'id' : 'references-tab'}):
         for p in div.find_all('p', attrs = {'class' : 'bibitemcls'}):
             for a in p.find_all('a'):
-                if a.has_attr('href') and re.search('doi.org', a['href']):
-                    rdoi = re.sub('.*doi.org\/', r', DOI: ', a['href'])
-                    a.replace_with(rdoi)
-
+                if a.has_attr('href'):
+                    if re.search('doi.org', a['href']):
+                        rdoi = re.sub('.*doi.org\/', r', DOI: ', a['href'])
+                        a.replace_with(rdoi)
+                    elif re.search('arxiv.org', a['href']):
+                        rdoi = re.sub('.*arxiv.org\/abs\/', r', arXiv: ', a['href'])
+                        a.replace_with(rdoi)
+                    elif re.search('(ams.org|zbmath.org|numdam.org)', a['href']):
+                        a.decompose()
+                        
             pt = p.text.strip()
             pt = re.sub(', Volume ', ' ', pt)
             #replace ';' in names by ','
             numbers = False
-            parts = re.split(';', pt)
+            parts = re.split(';', re.sub('[\n\t\r]', '', pt))
             ptnew = parts[0]
             if re.search('\d', re.sub('^[\[\]\d]+', '', ptnew)):
                 numbers = True
@@ -127,7 +154,7 @@ for rec in prerecs:
                     ptnew += ', ' + part
                 if re.search('\d', part):
                     numbers = True
-            rec['refs'].append([('x', ptnew)])
+            rec['refs'].append([('x', re.sub('\|', ' ', ptnew))])
     if not skipalreadyharvested or not rec['doi'] in alreadyharvested:
         ejlmod3.printrecsummary(rec)
         recs.append(rec)
